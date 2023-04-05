81ecc2b4ad0ae335f425d739baa8d2cefd50435a
Òscar Casajuana <elboletaire@gmail.com>
2023-04-05 15:47:47 +0200

    Replace rainbowkit dropdown with a custom one
    closes #21
    
    It may need an additional iteration over the styles, but requires
    changes from `@vocdoni/react-components` yet to be published.

---
+++ src/components/Navbar/Account.tsx
@@ -0,0 +1,35 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Balance } from '@vocdoni/react-components'
+import { useTranslation } from 'react-i18next'
+import { BiChevronDown, BiCoinStack, BiLogOut } from 'react-icons/bi'
+import { useAccount, useDisconnect } from 'wagmi'
+
+const addressTextOverflow = (address: string) =>
+  `${address.substring(0, 6)}...${address.substring(address.length - 4, address.length)}`
+
+export const Account = () => {
+  const { isConnected, address } = useAccount()
+  const { disconnect } = useDisconnect()
+  const { t } = useTranslation()
+
+  if (!isConnected) {
+    return <ConnectButton chainStatus='none' showBalance={false} label={t('menu.connect').toString()} />
+  }
+
+  return (
+    <Menu>
+      <MenuButton as={Button} variant='ghost' rightIcon={<BiChevronDown />}>
+        {addressTextOverflow(address as string)}
+      </MenuButton>
+      <MenuList>
+        <MenuItem icon={<BiCoinStack />}>
+          <Balance />
+        </MenuItem>
+        <MenuItem icon={<BiLogOut />} onClick={() => disconnect()}>
+          {t('logout')}
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
