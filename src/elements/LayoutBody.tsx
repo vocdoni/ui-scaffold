4bd4f262355c3d54f343a7f067937374bb44f2a7
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-04-11 16:10:57 +0200

    Fix toast width (#30)
    Co-authored-by: Òscar Casajuana <elboletaire@gmail.com>

--- src/elements/LayoutBody.tsx
+++ src/elements/LayoutBody.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 const LayoutBody = () => (
   <Box as='main' py={14}>
-    <Box maxWidth={300} margin='0 auto' paddingX={{ base: 0, sm: 4 }}>
+    <Box maxWidth={304} margin='0 auto' paddingX={{ base: 0, sm: 4 }}>
       <Outlet />
     </Box>
   </Box>
