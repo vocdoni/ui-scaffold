c6e88e799a42a456593446281d5f0ccafb0466bf
Òscar Casajuana <elboletaire@gmail.com>
2023-04-12 17:17:34 +0200

    Created custom fake chain to properly bypass switch chain behavior
    closes #21

--- src/constants/rainbow.ts
+++ src/constants/rainbow.ts
@@ -1,10 +1,19 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 import { configureChains, createClient } from 'wagmi'
+import type { Chain } from 'wagmi/chains'
 import { mainnet } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
-export const { chains, provider } = configureChains([mainnet], [publicProvider()])
+const vocdoni = {
+  ...mainnet,
+  // we need id zero to bypass the switch chain behavior
+  id: 0,
+  name: 'Vocdoni',
+  network: 'none',
+} as const satisfies Chain
+
+export const { chains, provider } = configureChains([vocdoni], [publicProvider()])
 
 export const { connectors } = getDefaultWallets({
   appName: "Vocdoni's Voting Protocol",
