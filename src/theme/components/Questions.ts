276ca090b4caa9537c8be550f864e2975a797e51
Gerard <g.roura.v@protonmail.com>
2023-05-04 11:12:12 +0200

    QuestionsForm styles

--- src/theme/components/Questions.ts
+++ src/theme/components/Questions.ts
@@ -5,17 +5,58 @@ const { defineMultiStyleConfig, definePartsStyle } = createMultiStyleConfigHelpe
 
 const baseStyle = definePartsStyle({
   question: {
-    bg: 'white',
-    borderRadius: 5,
-    padding: 5,
+    mb: '50px',
+
+    '& > div': {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 4,
+    },
+  },
+  title: {
+    mb: 0,
   },
 
   description: {
-    marginTop: 4,
-    color: 'gray',
+    color: 'process.description',
+
+    '& p': {
+      mb: 0,
+    },
   },
-  radioGroup: {
-    color: 'gray',
+
+  stack: {
+    minW: 72,
+    maxW: 96,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 3,
+
+    '& label': {
+      position: 'relative',
+      p: 0,
+
+      '& input:checked + span': {
+        color: 'process.btn_active',
+        border: 'none',
+        bgColor: 'process.btn_active',
+      },
+
+      '& span:nth-of-type(2)': {
+        position: 'absolute',
+        m: 0,
+        w: '100%',
+      },
+    },
+  },
+
+  radio: {
+    position: 'relative',
+    w: '100%',
+    height: 6,
+    border: 'none',
+    boxShadow: '2px 2px 6px rgba(0, 0, 0, 0.1)',
+    borderRadius: 6,
   },
 })
 
