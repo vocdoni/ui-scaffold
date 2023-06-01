37aa70539f699d64bf4d67e2f2709d3ee33586f4
Òscar Casajuana <elboletaire@gmail.com>
2023-06-01 20:58:23 +0200

    refs #55 Recover balance in navbar (only for existing accounts)

--- src/components/Navbar/Account.tsx
+++ src/components/Navbar/Account.tsx
@@ -1,9 +1,10 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useClient } from '@vocdoni/chakra-components'
+import { Balance, useClient } from '@vocdoni/chakra-components'
 import { useTranslation } from 'react-i18next'
 import { BiChevronDown, BiLogOut } from 'react-icons/bi'
 import { useAccount, useDisconnect } from 'wagmi'
+import { useAccountHealthTools } from '../Account/use-account-health-tools'
 
 const addressTextOverflow = (address: string) =>
   `${address.substring(0, 6)}...${address.substring(address.length - 4, address.length)}`
@@ -11,6 +12,7 @@ const addressTextOverflow = (address: string) =>
 export const Account = () => {
   const { isConnected, address } = useAccount()
   const { clear } = useClient()
+  const { exists } = useAccountHealthTools()
   const { disconnect } = useDisconnect()
   const { t } = useTranslation()
 
@@ -24,6 +26,11 @@ export const Account = () => {
         {addressTextOverflow(address as string)}
       </MenuButton>
       <MenuList>
+        {isConnected && exists && (
+          <MenuItem>
+            <Balance />
+          </MenuItem>
+        )}
         <MenuItem
           icon={<BiLogOut />}
           onClick={() => {
