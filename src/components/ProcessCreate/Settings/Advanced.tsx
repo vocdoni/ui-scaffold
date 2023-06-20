b3c986ab0780b11f5bf4cd3bba3124a9a8adaebc
Òscar Casajuana <elboletaire@gmail.com>
2023-06-20 12:21:31 +0200

    Properly manage boolean radio groups in forms

--- src/components/ProcessCreate/Settings/Advanced.tsx
+++ src/components/ProcessCreate/Settings/Advanced.tsx
@@ -1,48 +1,39 @@
 import { Checkbox, Flex, FormControl, FormLabel, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
+import { useBooleanRadioRegister } from '../../../constants'
 
 const SettingsAdvanced = () => {
   const { t } = useTranslation()
-  const { register, setValue, watch } = useFormContext()
-
-  const [valueRadio, setValueRadio] = useState('1')
-
-  console.log(watch())
-
-  useEffect(() => {
-    if (valueRadio === '1') setValue('electionType.secretUntilTheEnd', false)
-    else setValue('electionType.secretUntilTheEnd', true)
-  }, [valueRadio, setValue])
+  const { register } = useFormContext()
 
   return (
     <Flex flexDirection='column' gap={2}>
       <Flex alignItems='center' gap={1}>
-        <Text as='span' fontWeight={700} fontSize='xl'>
+        <Text as='span' fontWeight='bold' fontSize='xl'>
           {t('form.process_create.settings.settings')}
         </Text>
         <Text as='span' color='process_create.description' fontSize='xs'>
           ({t('form.process_create.settings.settings_description')})
         </Text>
       </Flex>
-      <RadioGroup onChange={setValueRadio} value={valueRadio} mb={3}>
+      <RadioGroup mb={3} {...useBooleanRadioRegister('electionType.secretUntilTheEnd')}>
         <Stack direction='column'>
           <Flex flexDirection='column'>
-            <Radio value='1'>
-              <Text as='span' ml={1} fontSize='sm' fontWeight={700}>
-                {t('form.process_create.settings.real_time')}:
-              </Text>{' '}
+            <Radio value='0'>
+              <Text as='span' ml={1} fontWeight='bold' fontSize='sm'>
+                {t('form.process_create.settings.real_time')}
+              </Text>
               <Text as='span' ml={1} fontSize='xs' color='process_create.description'>
                 {t('form.process_create.settings.real_time_description')}
               </Text>
             </Radio>
           </Flex>
           <Flex flexDirection='column'>
-            <Radio value='2'>
-              <Text as='span' ml={1} fontSize='sm' fontWeight={700}>
+            <Radio value='1'>
+              <Text as='span' ml={1} fontWeight='bold' fontSize='sm'>
                 {t('form.process_create.settings.secret')}
-              </Text>{' '}
+              </Text>
               <Text as='span' ml={1} fontSize='xs' color='process_create.description'>
                 {t('form.process_create.settings.secret_description')}
               </Text>
@@ -53,9 +44,9 @@ const SettingsAdvanced = () => {
       <FormControl display='flex' alignItems='center'>
         <Checkbox {...register('voteOverwrites')} />
         <FormLabel whiteSpace='nowrap' mb='0' ml={3}>
-          <Text as='span' fontSize='xs' fontWeight={700}>
-            {t('form.process_create.settings.vote_overwrites')}:{' '}
-            <Text as='span' fontSize='xs' fontWeight={400} color='process_create.description'>
+          <Text as='span' fontSize='xs' fontWeight='bold'>
+            {t('form.process_create.settings.vote_overwrites')}
+            <Text as='span' ml={1} fontSize='xs' fontWeight='normal' color='process_create.description'>
               {t('form.process_create.settings.vote_overwrites_descripton')}
             </Text>
           </Text>
@@ -64,9 +55,9 @@ const SettingsAdvanced = () => {
       <FormControl display='flex' alignItems='center'>
         <Checkbox {...register('electionType.interruptible')} />
         <FormLabel whiteSpace='nowrap' mb='0' ml={3}>
-          <Text as='span' fontSize='xs' fontWeight={700}>
-            {t('form.process_create.settings.interruptible')}:{' '}
-            <Text as='span' fontSize='xs' fontWeight={400} color='process_create.description'>
+          <Text as='span' fontSize='xs' fontWeight='bold'>
+            {t('form.process_create.settings.interruptible')}
+            <Text as='span' ml={1} fontSize='xs' fontWeight='normal' color='process_create.description'>
               {t('form.process_create.settings.interruptible_description')}
             </Text>
           </Text>
