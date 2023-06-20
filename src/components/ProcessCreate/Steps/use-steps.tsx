c43ef73c3b61100264eb0280cfdee2092bacd80f
Gerard <g.roura.v@protonmail.com>
2023-06-20 17:06:29 +0200

    fixed issue doble census

--- src/components/ProcessCreate/Steps/use-steps.tsx
+++ src/components/ProcessCreate/Steps/use-steps.tsx
@@ -39,7 +39,6 @@ export const useProcessCreationSteps = () => {
 export const useStepContents = () => {
   const { t } = useTranslation()
   const steps = [
-    { title: t('form.process_create.steps.census'), Contents: Census },
     { title: t('form.process_create.steps.checks'), Contents: Checks },
     { title: t('form.process_create.steps.info'), Contents: Info, first: true },
     { title: t('form.process_create.steps.questions'), Contents: Questions },
