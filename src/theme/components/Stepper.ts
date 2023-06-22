4988935919532531ca0a7b852d33b4734d431ce9
Gerard <g.roura.v@protonmail.com>
2023-06-22 08:58:00 +0200

    closes #76, Issue warning about shorthand resolved

--- src/theme/components/Stepper.ts
+++ src/theme/components/Stepper.ts
@@ -1,6 +1,17 @@
 const baseStyle = {
   separator: {
     marginTop: 2,
+    '&[data-status=complete]': {
+      background: 'process_create.stepper',
+    },
+  },
+  indicator: {
+    '&[data-status=complete]': {
+      background: 'process_create.stepper',
+    },
+    '&[data-status=active]': {
+      borderColor: 'process_create.stepper',
+    },
   },
 }
 
