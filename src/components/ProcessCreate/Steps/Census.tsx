bec50a2e65192ff528864b3748a11cd1826f0ac3
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-06-26 11:51:25 +0200

    [minor] style changes (#88)

--- src/components/ProcessCreate/Steps/Census.tsx
+++ src/components/ProcessCreate/Steps/Census.tsx
@@ -47,12 +47,12 @@ export const Census = () => {
 
             <TabPanels>
               <TabPanel>
-                <Text textAlign='center' color='#00DAAE'>
+                <Text textAlign='center' color='brand'>
                   TO DO
                 </Text>
               </TabPanel>
               <TabPanel>
-                <Text textAlign='center' color='#00DAAE'>
+                <Text textAlign='center' color='brand'>
                   TO DO
                 </Text>
               </TabPanel>
