fb2d0f7e66c9dc6279e52037257c67f5f0b0f672
Gerard <g.roura.v@protonmail.com>
2023-06-28 12:48:14 +0200

    addAddress and CensusWeb3Addresses in the same form

--- src/components/ProcessCreate/Census/Web3.tsx
+++ src/components/ProcessCreate/Census/Web3.tsx
@@ -10,14 +10,35 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react'
+import { enforceHexPrefix, useClient } from '@vocdoni/chakra-components'
+import { useEffect, useState } from 'react'
 import { useFieldArray, useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { addressTextOverflow, fieldMapErrorMessage, isInvalidFieldMap } from '../../../constants'
+import { useProcessCreationSteps } from '../Steps/use-steps'
 
 export const CensusWeb3Addresses = () => {
+  const { t } = useTranslation()
+  const { account } = useClient()
+  const { form, setForm } = useProcessCreationSteps()
+
+  const {
+    register,
+    formState: { errors },
+    watch,
+    setValue,
+    trigger,
+    resetField,
+  } = useFormContext()
+
   const { fields, remove } = useFieldArray({
     name: 'addresses',
   })
+
+  const addresses = watch('addresses')
+  const newAddress = watch('newAddress')
+
+  const [initialized, setInitialized] = useState(!!addresses.length)
 
   const value = useBreakpointValue({
     base: 6,
@@ -25,77 +46,83 @@ export const CensusWeb3Addresses = () => {
     md: null,
   })
 
-  return (
-    <Flex
-      flexDirection='column'
-      gap={3}
-      maxW='490px'
-      height='400px'
-      borderRadius='lg'
-      border='1px solid'
-      borderColor='process_create.border'
-      mx='auto'
-      overflowY='scroll'
-      bgColor='process_create.input.bg'
-    >
-      {fields.map((address, index) => (
-        <Flex
-          key={address.id}
-          justifyContent='start'
-          alignItems='center'
-          gap={2}
-          mx='auto'
-          borderBottom='1px solid'
-          borderColor='process_create.border'
-          w='full'
-          p={5}
-        >
-          <Text fontWeight='bold'>{index + 1}</Text>
-          <Text>{addressTextOverflow((address as any).address, value)}</Text>
+  useEffect(() => {
+    setForm({ ...form, addresses })
+    if (account?.address && !initialized && addresses.length === 0) {
+      setValue('addresses', [{ address: enforceHexPrefix(account.address), weight: 0 }])
+      setInitialized(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [account?.address, addresses, initialized])
 
-          <IconButton
-            size='xs'
-            type='button'
-            icon={<DeleteIcon />}
-            aria-label='delete option'
-            onClick={() => remove(index)}
-            ml='auto'
+  const handleAddAddress = async () => {
+    await trigger()
+    // Trigger form validation
+
+    if (!errors.newAddress) {
+      // Perform any necessary actions
+      setValue('addresses', [...addresses, { address: newAddress, weight: 0 }])
+      resetField('newAddress')
+    }
+  }
+
+  return (
+    <>
+      <FormControl isInvalid={isInvalidFieldMap(errors, 'newAddress')} display='flex' justifyContent='center' gap={2}>
+        <Box>
+          <Input
+            {...register('newAddress', {
+              pattern: {
+                value: /^(0x)?[0-9a-fA-F]{40}$/,
+                message: t('form.error.address_pattern'),
+              },
+            })}
+            w={{ base: '200px', sm: '300px', md: '420px' }}
           />
-        </Flex>
-      ))}
-    </Flex>
-  )
-}
+          <FormErrorMessage>{fieldMapErrorMessage(errors, 'newAddress')}</FormErrorMessage>
+        </Box>
+        <Button type='button' ml='none' onClick={handleAddAddress}>
+          Add
+        </Button>
+      </FormControl>
+      <Flex
+        flexDirection='column'
+        gap={3}
+        maxW='490px'
+        height='400px'
+        borderRadius='lg'
+        border='1px solid'
+        borderColor='process_create.border'
+        mx='auto'
+        overflowY='scroll'
+        bgColor='process_create.input.bg'
+      >
+        {fields.map((address, index) => (
+          <Flex
+            key={address.id}
+            justifyContent='start'
+            alignItems='center'
+            gap={2}
+            mx='auto'
+            borderBottom='1px solid'
+            borderColor='process_create.border'
+            w='full'
+            p={5}
+          >
+            <Text fontWeight='bold'>{index + 1}</Text>
+            <Text>{addressTextOverflow((address as any).address, value)}</Text>
 
-export const CensusWeb3AddressesAddAddress = () => {
-  const { t } = useTranslation()
-
-  const {
-    formState: { errors },
-    register,
-  } = useFormContext()
-
-  return (
-    <FormControl isInvalid={isInvalidFieldMap(errors, 'newAddress')} display='flex' justifyContent='center' gap={2}>
-      <Box>
-        <Input
-          {...register('newAddress', {
-            required: {
-              value: true,
-              message: t('form.error.field_is_required'),
-            },
-            pattern: {
-              value: /^(0x)?[0-9a-fA-F]{40}$/,
-              message: t('form.error.address_pattern'),
-            },
-          })}
-          w={{ base: '200px', sm: '300px', md: '420px' }}
-        />
-        <FormErrorMessage>{fieldMapErrorMessage(errors, 'newAddress')}</FormErrorMessage>
-      </Box>
-      <Button type='submit' ml='none'>
-        Add
-      </Button>
-    </FormControl>
+            <IconButton
+              size='xs'
+              type='button'
+              icon={<DeleteIcon />}
+              aria-label='delete option'
+              onClick={() => remove(index)}
+              ml='auto'
+            />
+          </Flex>
+        ))}
+      </Flex>
+    </>
   )
 }
--- src/components/ProcessCreate/Steps/CensusWeb3.tsx
+++ src/components/ProcessCreate/Steps/CensusWeb3.tsx
@@ -1,9 +1,7 @@
 import { Box, Text } from '@chakra-ui/react'
-import { enforceHexPrefix, useClient } from '@vocdoni/chakra-components'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
-import { useTranslation } from 'react-i18next'
-import { CensusWeb3Addresses, CensusWeb3AddressesAddAddress } from '../Census/Web3'
+import { CensusWeb3Addresses } from '../Census/Web3'
 import { useProcessCreationSteps } from './use-steps'
 
 export interface CensusWeb3Values {
@@ -16,70 +14,28 @@ export interface Web3Address {
 }
 
 export const StepsCensusWeb3 = () => {
-  const { t } = useTranslation()
-  const { account } = useClient()
   const { form, setForm, next } = useProcessCreationSteps()
-  const methods = useForm<CensusWeb3Values>({
+  const methods = useForm({
     defaultValues: {
       addresses: form.addresses,
-    },
-  })
-
-  const methodsAddAddress = useForm<{ newAddress: string }>({
-    defaultValues: {
       newAddress: '',
     },
   })
 
   const addresses = methods.watch('addresses')
 
-  const [initialized, setInitialized] = useState(!!addresses.length)
-
   useEffect(() => {
     setForm({ ...form, addresses })
-    if (account?.address && !initialized && addresses.length === 0) {
-      methods.setValue('addresses', [{ address: enforceHexPrefix(account.address), weight: 0 }])
-      setInitialized(true)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [account?.address, addresses, initialized])
 
-  useEffect(() => {
-    if (methods.formState.errors.addresses?.type === 'manual') {
-      methods.clearErrors('addresses')
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [addresses])
 
-  const onSubmitAddAddress = (data: { newAddress: string }) => {
-    methods.setValue('addresses', [...methods.getValues().addresses, { address: data.newAddress, weight: 0 }])
-    methodsAddAddress.reset()
-  }
-
   const onSubmit: SubmitHandler<CensusWeb3Values> = (data) => {
-    if (!data.addresses.length) {
-      methods.setError('addresses', {
-        type: 'manual',
-        message: t('form.process_create.census.web3_min_address'),
-      })
-    } else {
-      setForm({ ...form, ...data })
-      next()
-    }
+    setForm({ ...form, addresses: data.addresses })
+    next()
   }
   return (
     <>
-      <Box mb={5}>
-        <Text fontWeight='bold' fontSize='2xl' textAlign='center' mb={3}>
-          {t('form.process_create.census.web3_title')}
-        </Text>
-        <FormProvider {...methodsAddAddress}>
-          <Box as='form' onSubmit={methodsAddAddress.handleSubmit(onSubmitAddAddress)}>
-            <CensusWeb3AddressesAddAddress />
-          </Box>
-        </FormProvider>
-      </Box>
-
       <FormProvider {...methods}>
         <Box as='form' id='process-create-form' onSubmit={methods.handleSubmit(onSubmit)}>
           <CensusWeb3Addresses />
