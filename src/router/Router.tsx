996d38795acc5416ef955db39a1def9ff96babf8
Òscar Casajuana <elboletaire@gmail.com>
2023-06-30 16:06:33 +0200

    refs #107 Avoid using fragment in the routes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -22,46 +22,44 @@ export const RoutesProvider = () => {
 
   const router = createHashRouter(
     createRoutesFromElements(
-      <>
-        <Route path='/' element={<Layout />}>
-          <Route errorElement={<Error />}>
-            <Route element={<LayoutHome />}>
-              <Route
-                index
-                element={
-                  <SuspenseLoader>
-                    <Home />
-                  </SuspenseLoader>
-                }
-              />
-            </Route>
-            <Route element={<LayoutContents />}>
-              <Route
-                path='processes/:id'
-                element={
-                  <SuspenseLoader>
-                    <Process />
-                  </SuspenseLoader>
-                }
-                loader={async ({ params }) => client.fetchElection(params.id)}
-              />
-              <Route
-                path='organization/:address'
-                element={
-                  <SuspenseLoader>
-                    <Organization />
-                  </SuspenseLoader>
-                }
-              />
-              <Route
-                path='*'
-                element={
-                  <SuspenseLoader>
-                    <NotFound />
-                  </SuspenseLoader>
-                }
-              />
-            </Route>
+      <Route path='/'>
+        <Route errorElement={<Error />} element={<Layout />}>
+          <Route element={<LayoutHome />}>
+            <Route
+              index
+              element={
+                <SuspenseLoader>
+                  <Home />
+                </SuspenseLoader>
+              }
+            />
+          </Route>
+          <Route element={<LayoutContents />}>
+            <Route
+              path='processes/:id'
+              element={
+                <SuspenseLoader>
+                  <Process />
+                </SuspenseLoader>
+              }
+              loader={async ({ params }) => client.fetchElection(params.id)}
+            />
+            <Route
+              path='organization/:address'
+              element={
+                <SuspenseLoader>
+                  <Organization />
+                </SuspenseLoader>
+              }
+            />
+            <Route
+              path='*'
+              element={
+                <SuspenseLoader>
+                  <NotFound />
+                </SuspenseLoader>
+              }
+            />
           </Route>
         </Route>
         <Route element={<LayoutProcessCreate />}>
@@ -82,7 +80,7 @@ export const RoutesProvider = () => {
             />
           </Route>
         </Route>
-      </>
+      </Route>
     )
   )
 
