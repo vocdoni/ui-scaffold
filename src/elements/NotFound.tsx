45134c3c9d373e7e31c282c284d40e1d804eeda0
Òscar Casajuana <elboletaire@gmail.com>
2023-07-03 19:24:46 +0200

    [minor] translated 404 page

--- src/elements/NotFound.tsx
+++ src/elements/NotFound.tsx
@@ -1,17 +1,26 @@
-import { Box, Text } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Box, Link, Text } from '@chakra-ui/react'
+import { Trans, useTranslation } from 'react-i18next'
+import { Link as RLink } from 'react-router-dom'
 
 const NotFound = () => {
+  const { t } = useTranslation()
   return (
     <Box textAlign='center' mt={12}>
       <Text as='h2' fontSize='2em'>
-        Page not found
+        {t('not_found')}
       </Text>
       <Text>
-        Go to
-        <Text display='inline' ml={1} textDecoration='underline'>
-          <Link to='/'>Home</Link>
-        </Text>
+        <Trans
+          i18nKey='go_to_home'
+          components={{
+            root: <Link as={RLink} to='/' />,
+          }}
+        >
+          Go to{' '}
+          <Link as={RLink} to='/'>
+            home
+          </Link>
+        </Trans>
       </Text>
     </Box>
   )
