97f663f49df0d86a2c3cc3e5a51d4760fb9072c4
Òscar Casajuana <elboletaire@gmail.com>
2023-07-04 11:02:58 +0200

    Typo fixes and minor ls change

--- src/components/Layout/Disclaimer.tsx
+++ src/components/Layout/Disclaimer.tsx
@@ -11,14 +11,14 @@ import {
 import { useState } from 'react'
 
 export const Disclaimer = () => {
-  const [closed, setClosed] = useState(window.localStorage.getItem('vocdoni-disclaimer-modal'))
+  const [closed, setClosed] = useState(window.localStorage.getItem('vocdoni.disclaimer-modal'))
 
   return (
     <Modal
       isOpen={!closed}
       onClose={() => {
         setClosed('yup')
-        window.localStorage.setItem('vocdoni-disclaimer-modal', 'yup')
+        window.localStorage.setItem('vocdoni.disclaimer-modal', 'yup')
       }}
       size='4xl'
     >
@@ -29,16 +29,16 @@ export const Disclaimer = () => {
         <ModalBody pb={6}>
           <Stack gap={3}>
             <p>
-              This is a DEMO frontend build for showcase purposes. All the data in this homepage are placeholders. You
-              can navigate and create elections, to better understand how to use the Vocdoni tech stack.
+              This is a DEMO frontend build for showcase purposes. All the data on this homepage is placeholder. You can
+              navigate and create elections to better understand how to use the Vocdoni tech stack.
             </p>
-            <p>Buildi with ❤️ by Vocdoni using: </p>
+            <p>Built with ❤️ by Vocdoni using:</p>
             <p>
               <Link href='https://github.com/vocdoni/ui-components' fontWeight='bold'>
                 UI-Components
               </Link>
               : React components made with Chakra. This project aims to ease the creation of voting user interfaces,
-              allowing developers to easily style everything as desired, and to chose from a variety of different voting
+              allowing developers to easily style everything as desired and choose from a variety of different voting
               flow components.
             </p>
             <p>
@@ -46,7 +46,7 @@ export const Disclaimer = () => {
                 SDK
               </Link>
               : A Typescript library. The Vocdoni SDK is a convenient way to interact with the Vocdoni Protocol through
-              the new API, allowing anyone to create, manage and participate in voting processes and collective
+              the new API, allowing anyone to create, manage, and participate in voting processes and collective
               decision-making.
             </p>
           </Stack>
