107ecd002e346aac68019d5097015d23a63e49c2
Gerard <g.roura.v@protonmail.com>
2023-07-11 13:05:44 +0200

    header and body min height 100vh, closes #138

--- src/elements/Layout.tsx
+++ src/elements/Layout.tsx
@@ -1,30 +1,32 @@
-import { Box, Flex, HStack } from '@chakra-ui/react'
+import { Box, HStack } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 const Layout = () => (
-  <Flex direction='column' minH='100vh'>
-    <HStack
-      as='header'
-      position='sticky'
-      top={0}
-      zIndex={20}
-      h={18}
-      bgColor='navbar.bg'
-      boxShadow='var(--box-shadow-navbar)'
-    >
-      <Navbar w='full' maxW={480} mx='auto' px={{ base: 2, sm: 4 }} />
-    </HStack>
-    <Box as='main' pt={8} pb={40}>
-      <Box maxWidth={440} m='0 auto' px={{ base: 2, sm: 4 }}>
-        <Outlet />
+  <>
+    <Box minH='100vh'>
+      <HStack
+        as='header'
+        position='sticky'
+        top={0}
+        zIndex={20}
+        h={18}
+        bgColor='navbar.bg'
+        boxShadow='var(--box-shadow-navbar)'
+      >
+        <Navbar w='full' maxW={480} mx='auto' px={{ base: 2, sm: 4 }} />
+      </HStack>
+      <Box as='main' pt={8} pb={40}>
+        <Box maxWidth={440} m='0 auto' px={{ base: 2, sm: 4 }}>
+          <Outlet />
+        </Box>
       </Box>
     </Box>
     <Box as='footer' mt='auto'>
       <Footer maxWidth={480} mx='auto' px={{ base: 2, sm: 4 }} />
     </Box>
-  </Flex>
+  </>
 )
 
 export default Layout
