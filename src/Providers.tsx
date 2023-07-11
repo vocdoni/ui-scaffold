d52338a3d7709e2f16476ae3534207580d872cef
Gerard <g.roura.v@protonmail.com>
2023-07-11 13:14:26 +0200

    removed input search header, closes #140

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -3,7 +3,6 @@ import { Signer } from '@ethersproject/abstract-signer'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { ClientProvider } from '@vocdoni/chakra-components'
 import { EnvOptions } from '@vocdoni/sdk'
-import { Dispatch, SetStateAction, createContext, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { WagmiConfig, useSigner } from 'wagmi'
 import { VocdoniEnvironment } from './constants'
@@ -12,16 +11,6 @@ import { translations } from './i18n/components'
 import { datesLocale } from './i18n/locales'
 import { RoutesProvider } from './router/Router'
 import { rainbowStyles, theme } from './theme'
-
-export type SearchInputContextProps = {
-  isSearchInScreen: boolean
-  setIsSearchInScreen: Dispatch<SetStateAction<boolean>>
-  isFullInput: boolean
-  displayFullInput: () => void
-  removeFullInput: () => void
-}
-
-export const SearchInputContext = createContext<SearchInputContextProps | undefined>(undefined)
 
 export const Providers = () => (
   <ChakraProvider theme={extendTheme(theme)}>
@@ -36,13 +25,6 @@ export const AppProviders = () => {
   const { colorMode } = useColorMode()
   const { t, i18n } = useTranslation()
 
-  const [isSearchInScreen, setIsSearchInScreen] = useState(true)
-  const [isFullInput, setIsFullInput] = useState(false)
-  const displayFullInput = () => setIsFullInput(true)
-  const removeFullInput = () => setIsFullInput(false)
-
-  const searchInputValues = { isSearchInScreen, setIsSearchInScreen, isFullInput, displayFullInput, removeFullInput }
-
   return (
     <RainbowKitProvider chains={chains} theme={rainbowStyles(colorMode)}>
       <ClientProvider
@@ -51,10 +33,8 @@ export const AppProviders = () => {
         locale={translations(t)}
         datesLocale={datesLocale(i18n.language)}
       >
-        <SearchInputContext.Provider value={searchInputValues}>
-          <ColorModeScript />
-          <RoutesProvider />
-        </SearchInputContext.Provider>
+        <ColorModeScript />
+        <RoutesProvider />
       </ClientProvider>
     </RainbowKitProvider>
   )
