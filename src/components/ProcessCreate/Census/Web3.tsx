ed9b372f8e13b2f4214ecdfb7f77ecd8251a163a
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-07-12 16:06:11 +0200

    prevent adding same address (#154)
    closes #153

--- src/components/ProcessCreate/Census/Web3.tsx
+++ src/components/ProcessCreate/Census/Web3.tsx
@@ -60,6 +60,11 @@ export const CensusWeb3Addresses = () => {
     // Trigger form validation
     await trigger()
 
+    const duplicateAddress = addresses.find((add: any) => add.address === newAddress)
+
+    if (duplicateAddress) {
+      return setError('newAddress', { type: 'custom', message: t('form.error.address_already_in_use') })
+    }
     if (!newAddress) {
       return setError('newAddress', { type: 'custom', message: t('form.error.field_is_required') })
     }
