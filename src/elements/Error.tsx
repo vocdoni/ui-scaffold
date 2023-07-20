0b39294e5dd21b6a3448eee0dd89832a3ed6d833
Gerard <g.roura.v@protonmail.com>
2023-07-20 11:49:06 +0200

    page not found and error

--- src/elements/Error.tsx
+++ src/elements/Error.tsx
@@ -1,47 +1,16 @@
-import { Box, Flex, Link, Text } from '@chakra-ui/react'
-import { useTranslation } from 'react-i18next'
+import { WarningIcon } from '@chakra-ui/icons'
+import { Flex, Text } from '@chakra-ui/react'
 import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
-  const error = useRouteError() as Error
+  const error = useRouteError()
   console.error(error)
-  const { t } = useTranslation()
 
   return (
-    <Box>
-      <Box position='relative' minH='400px' mb={10} pt={5}>
-        <Text
-          fontSize={{ base: 'xl11', sm: 'xl12' }}
-          lineHeight={0.9}
-          textAlign='center'
-          p={0}
-          bgGradient='var(--vcd-gradient-primary)'
-          bgClip='text'
-          fontWeight='bold'
-        >
-          404
-        </Text>
-        <Flex
-          position='absolute'
-          margin-left='auto'
-          margin-right='auto'
-          left={0}
-          right={0}
-          zIndex={10}
-          justifyContent='center'
-          mx='auto'
-          maxW={{ base: 84, sm: 112, lg: 160 }}
-          top={20}
-        >
-          <Text textAlign='center' fontWeight={700} fontSize={{ base: 'xl4', sm: 'xl5', lg: 'xl7' }}>
-            {t('error_text')}
-          </Text>
-        </Flex>
-      </Box>
-      <Box textAlign='center'>
-        <Link>{t('error.return_to_home')}</Link>
-      </Box>
-    </Box>
+    <Flex flexDirection='column' gap={4} alignItems='center' mt={12}>
+      <WarningIcon />
+      <Text>Error loading the page</Text>
+    </Flex>
   )
 }
 
--- src/elements/NotFound.tsx
+++ src/elements/NotFound.tsx
@@ -1,28 +1,38 @@
-import { Box, Link, Text } from '@chakra-ui/react'
-import { Trans, useTranslation } from 'react-i18next'
-import { Link as RLink } from 'react-router-dom'
+import { Flex, Link, Text } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
+
+  const { idx } = window.history.state
+
   return (
-    <Box textAlign='center' mt={12}>
-      <Text as='h2' fontSize='2em'>
+    <Flex flexDirection='column' justifyContent='center' alignItems='center' pt={20}>
+      <Text
+        fontSize={{ base: 'xl11', sm: 'xl12' }}
+        lineHeight={0.9}
+        textAlign='center'
+        p={0}
+        bgGradient='var(--vcd-gradient-primary)'
+        bgClip='text'
+        fontWeight='bold'
+      >
+        404
+      </Text>
+      <Text
+        textAlign='center'
+        maxW={{ base: 84, sm: 112, lg: 160 }}
+        fontWeight={700}
+        fontSize={{ base: 'xl4', sm: 'xl5', lg: 'xl7' }}
+      >
         {t('not_found')}
       </Text>
-      <Text>
-        <Trans
-          i18nKey='go_to_home'
-          components={{
-            root: <Link as={RLink} to='/' />,
-          }}
-        >
-          Go to{' '}
-          <Link as={RLink} to='/'>
-            home
-          </Link>
-        </Trans>
-      </Text>
-    </Box>
+      <Link onClick={() => (idx ? navigate(-1) : navigate('/'))}>
+        {idx ? t('error.go_back') : t('error.return_to_home')}
+      </Link>
+    </Flex>
   )
 }
 
