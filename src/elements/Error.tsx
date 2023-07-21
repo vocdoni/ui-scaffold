e955b5ef8e5c2750ded87bfa4d348e2c460cc157
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-07-21 11:04:25 +0200

    Swap back not found and error pages (#188)
    Co-authored-by: Òscar Casajuana <elboletaire@gmail.com>

--- src/elements/Error.tsx
+++ src/elements/Error.tsx
@@ -1,47 +1,29 @@
-import { Box, Flex, Link, Text } from '@chakra-ui/react'
+import { WarningIcon } from '@chakra-ui/icons'
+import { Flex, Text } from '@chakra-ui/react'
+import { ErrAccountNotFound, ErrAddressMalformed, ErrCantParseElectionID, ErrElectionNotFound } from '@vocdoni/sdk'
 import { useTranslation } from 'react-i18next'
 import { useRouteError } from 'react-router-dom'
+import NotFound from './NotFound'
 
 const Error = () => {
-  const error = useRouteError() as Error
-  console.error(error)
   const { t } = useTranslation()
+  const error = useRouteError()
+
+  if (
+    error instanceof ErrElectionNotFound ||
+    error instanceof ErrCantParseElectionID ||
+    error instanceof ErrAddressMalformed ||
+    error instanceof ErrAccountNotFound
+  ) {
+    return <NotFound />
+  }
 
   return (
-    <Box>
-      <Box position='relative' minH='400px' mb={10} pt={5}>
-        <Text
-          fontSize={{ base: 'xl11', sm: 'xl12' }}
-          lineHeight={0.9}
-          textAlign='center'
-          p={0}
-          bgGradient='var(--vcd-gradient-primary)'
-          bgClip='text'
-          fontWeight='bold'
-        >
-          404
-        </Text>
-        <Flex
-          position='absolute'
-          margin-left='auto'
-          margin-right='auto'
-          left={0}
-          right={0}
-          zIndex={10}
-          justifyContent='center'
-          mx='auto'
-          maxW={{ base: 84, sm: 112, lg: 160 }}
-          top={20}
-        >
-          <Text textAlign='center' fontWeight={700} fontSize={{ base: 'xl4', sm: 'xl5', lg: 'xl7' }}>
-            {t('error_text')}
-          </Text>
-        </Flex>
-      </Box>
-      <Box textAlign='center'>
-        <Link>{t('error.return_to_home')}</Link>
-      </Box>
-    </Box>
+    <Flex flexDirection='column' gap={4} alignItems='center' mt={12}>
+      <WarningIcon />
+      <Text>{t('error.loading_page')}</Text>
+      <Text>{(error as Error).toString()}</Text>
+    </Flex>
   )
 }
 
