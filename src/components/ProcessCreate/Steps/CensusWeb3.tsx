4a1fdaafb3395cefa84563d7bf282173c528613f
Gerard <g.roura.v@protonmail.com>
2023-07-25 17:31:18 +0200

    remove comments

--- src/components/ProcessCreate/Steps/CensusWeb3.tsx
+++ src/components/ProcessCreate/Steps/CensusWeb3.tsx
@@ -1,69 +1,3 @@
-// import { Box, Text } from '@chakra-ui/react'
-// import { enforceHexPrefix, useClient } from '@vocdoni/chakra-components'
-// import { useEffect, useState } from 'react'
-// import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
-// import { CensusWeb3Addresses } from '../Census/Web3'
-// import { useProcessCreationSteps } from './use-steps'
-
-// export interface CensusWeb3Values {
-//   addresses: Web3Address[]
-// }
-
-// export interface Web3Address {
-//   address: string
-//   weight: number
-// }
-
-// export const StepsCensusWeb3 = () => {
-//   const { form, setForm, next } = useProcessCreationSteps()
-//   const { account } = useClient()
-
-//   const methods = useForm({
-//     defaultValues: {
-//       addresses: form.addresses,
-//       newAddress: '',
-//     },
-//   })
-
-//   const addresses = methods.watch('addresses')
-
-//   const [initialized, setInitialized] = useState(!!addresses.length)
-
-//   useEffect(() => {
-//     setForm({ ...form, addresses })
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [addresses])
-
-//   const onSubmit: SubmitHandler<CensusWeb3Values> = (data) => {
-//     setForm({ ...form, addresses: data.addresses })
-//     next()
-//   }
-
-//   useEffect(() => {
-//     setForm({ ...form, addresses })
-//     if (account?.address && !initialized && addresses.length === 0) {
-//       methods.setValue('addresses', [{ address: enforceHexPrefix(account.address), weight: 0 }])
-//       setInitialized(true)
-//     }
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [account?.address, addresses, initialized])
-
-//   return (
-//     <>
-//       <FormProvider {...methods}>
-//         <Box as='form' id='process-create-form' onSubmit={methods.handleSubmit(onSubmit)}>
-//           <CensusWeb3Addresses />
-//           {methods.formState.errors.addresses && (
-//             <Text color='red' textAlign='center' mt={2}>
-//               {methods.formState.errors.addresses.message}
-//             </Text>
-//           )}
-//         </Box>
-//       </FormProvider>
-//     </>
-//   )
-// }
-
 import { Box, Text } from '@chakra-ui/react'
 import { useEffect } from 'react'
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
@@ -97,8 +31,6 @@ export const StepsCensusWeb3 = () => {
   }, [addresses])
 
   const onSubmit: SubmitHandler<CensusWeb3Values> = (data) => {
-    // setForm({ ...form, addresses: data.addresses })
-    console.log(addresses.length)
     if (!addresses.length) {
       methods.setError('addresses', {
         type: 'manual',
