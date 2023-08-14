18123afd397e2854b65ba9f2ed2b337654385782
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-08-14 19:02:48 +0200

    Organization styles changes (#226)

--- src/components/Organization/View.tsx
+++ src/components/Organization/View.tsx
@@ -1,11 +1,10 @@
-import { AddIcon } from '@chakra-ui/icons'
-import { Box, Button, Card, CardBody, Flex, Grid, GridItem, Link, Spinner, Text } from '@chakra-ui/react'
+import { Box, Card, CardBody, Link as ChakraLink, Flex, Grid, GridItem, Link, Spinner, Text } from '@chakra-ui/react'
 import Logo from '@components/Layout/Logo'
 import { useClient, useOrganization } from '@vocdoni/react-providers'
 import { InvalidElection, PublishedElection, areEqualHexStrings } from '@vocdoni/sdk'
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
-import { NavLink } from 'react-router-dom'
+import { Link as ReactRouterLink } from 'react-router-dom'
 import ProcessCardDetailed from '../Process/CardDetailed'
 import Header from './Header'
 
@@ -100,8 +99,8 @@ const OrganizationView = () => {
               <Box>
                 {areEqualHexStrings(account?.address, organization?.address) ? (
                   <>
-                    <Text>{t('organization.elections_list_empty.title')}</Text>
-                    <Text>{t('organization.elections_list_empty.description')}</Text>
+                    <Text textAlign='center'>{t('organization.elections_list_empty.title')}</Text>
+                    <Text textAlign='center'>{t('organization.elections_list_empty.description')}</Text>
                     <Text>
                       <Trans
                         i18nKey='organization.elections_list_empty.footer'
@@ -110,14 +109,13 @@ const OrganizationView = () => {
                         }}
                       />
                     </Text>
-                    <NavLink to='/processes/create'>
-                      <Button colorScheme='primary' rightIcon={<AddIcon />}>
-                        {t('menu.create')}
-                      </Button>
-                    </NavLink>
+
+                    <ChakraLink as={ReactRouterLink} to='/processes/create' variant='button' colorScheme='primary'>
+                      {t('menu.create')}
+                    </ChakraLink>
                   </>
                 ) : (
-                  <Text>{t('organization.elections_list_empty.not_owner')}</Text>
+                  <Text textAlign='center'>{t('organization.elections_list_empty.not_owner')}</Text>
                 )}
               </Box>
             </CardBody>
