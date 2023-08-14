04e87b4ca860d6dc0864d1c20af284ebe36597c7
Gerard <g.roura.v@protonmail.com>
2023-08-14 10:06:38 +0200

    1- Issue resolved: <p> inside <p> 2- Changed the i18n keys to p1 and p2 3- Layout issues resolved

--- src/components/ProcessCreate/Census/Spreadsheet/Preview.tsx
+++ src/components/ProcessCreate/Census/Spreadsheet/Preview.tsx
@@ -16,17 +16,25 @@ export const CsvPreview = ({ manager, upload }: CsvPreviewProps) => {
 
   return (
     <Box p={10} borderRadius='lg' bgColor='process_create.spreadsheet.preview_bg'>
-      <Flex alignItems='center' gap={3} mb={5}>
-        <Icon as={RiFileExcel2Line} boxSize={14} color='process_create.spreadsheet.file' />
-        <Box maxW='50%'>
-          <Text fontSize='lg' isTruncated>
-            {manager.file.name}
-          </Text>
-          <Text color='process_create.spreadsheet.total_rows_text'>
-            {t('form.process_create.spreadsheet_total_rows', { count: manager.data.length })}
-          </Text>
-        </Box>
-        <Button colorScheme='primary' variant='ghost' border='1px solid' {...upload}>
+      <Flex
+        alignItems='center'
+        flexDirection={{ base: 'column', xl: 'row' }}
+        justifyContent='space-between'
+        gap={3}
+        mb={5}
+      >
+        <Flex gap={2} flexDirection={{ base: 'column', xl: 'row' }} alignItems='center'>
+          <Icon as={RiFileExcel2Line} boxSize={14} color='process_create.spreadsheet.file' />
+          <Box>
+            <Text fontSize='lg' isTruncated>
+              {manager.file.name}
+            </Text>
+            <Text color='process_create.spreadsheet.total_rows_text'>
+              {t('form.process_create.spreadsheet_total_rows', { count: manager.data.length })}
+            </Text>
+          </Box>
+        </Flex>
+        <Button colorScheme='primary' variant='ghost' border='1px solid' flexShrink={0} {...upload}>
           {t('form.process_create.spreadsheet.preview.upload_new_list')}
         </Button>
       </Flex>
@@ -35,7 +43,7 @@ export const CsvPreview = ({ manager, upload }: CsvPreviewProps) => {
         <Badge variant='primary'>{t('form.process_create.spreadsheet.preview.title')}</Badge>
         <Text my={3}>{t('form.process_create.spreadsheet.preview.description')}</Text>
 
-        <Flex flexDirection='column' gap={3} w='50%' mx='auto'>
+        <Flex flexDirection='column' gap={3} w={{ base: '70%', md: '50%' }} mx='auto'>
           {manager.header.map((field) => (
             <FormControl key={field}>
               <FormLabel mb={0} textTransform='capitalize'>
