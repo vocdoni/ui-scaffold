83778e6da9c66ce445276997a4c13cab422c4403
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-08-14 19:02:48 +0200

    Organization styles changes (#226)

--- src/theme/components/Link.ts
+++ src/theme/components/Link.ts
@@ -4,6 +4,43 @@ const primary = defineStyle({
   color: 'link',
 })
 
+const button = defineStyle((props) => {
+  const { colorScheme } = props
+
+  const commonStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 2,
+    height: 10,
+    color: 'white',
+    bgColor: 'primary.main',
+    borderRadius: 'md',
+    px: 4,
+    fontWeight: 'semibold',
+    lineHeight: 1.2,
+
+    _hover: {
+      textDecoration: 'none',
+    },
+  }
+  if (colorScheme === 'primary') {
+    return {
+      ...commonStyles,
+      bgColor: `primary.500`,
+
+      _hover: {
+        ...commonStyles._hover,
+        bgColor: `primary.600`,
+      },
+
+      _active: {
+        bgColor: `primary.700`,
+      },
+    }
+  }
+  return commonStyles
+})
+
 export const Link = defineStyleConfig({
-  variants: { primary },
+  variants: { button, primary },
 })
