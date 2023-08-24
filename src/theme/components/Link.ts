de54e279c360e9f198867c7bce02bb9081a7afda
Gerard <g.roura.v@protonmail.com>
2023-08-24 15:46:07 +0200

    new header, closes #202

--- src/theme/components/Link.ts
+++ src/theme/components/Link.ts
@@ -1,46 +1,63 @@
 import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
 
 const primary = defineStyle({
-  color: 'link',
+  color: 'link.primary',
 })
 
-const button = defineStyle((props) => {
+const rounded = defineStyle((props) => {
   const { colorScheme } = props
 
-  const commonStyles = {
+  // Simulating the styles of a button variant rounded as we do in the "button" file, 
+  // but since Chakra doesn't default to passing a colorScheme to Links, we need to fallback to the base gray color of the buttons.
+  // By default the color is white and if we want to change ir we can use the color prop
+  return {
     display: 'flex',
     alignItems: 'center',
-    gap: 2,
+    gap: 1,
     height: 10,
+    borderRadius: 20,
+    px: 4,
+    fontWeight: 'semibold',
+    lineHeight: 1.2,
     color: 'white',
-    bgColor: 'primary.main',
-    borderRadius: 'md',
+    bgColor: colorScheme ? `${colorScheme}.500` : 'gray.100',
+
+    _hover: {
+      textDecoration: 'none',
+      bgColor: colorScheme ? `${colorScheme}.600` : 'gray.200',
+    },
+
+    _active: {
+      bgColor: colorScheme ? `${colorScheme}.700` : 'gray.300',
+    },
+  }
+})
+
+const roundedGhost = defineStyle((props) => {
+  const { colorScheme } = props
+
+  //Similar to the case of "rounded" but without bgColor as a general style.
+  return {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 1,
+    height: 10,
+    borderRadius: 20,
     px: 4,
     fontWeight: 'semibold',
     lineHeight: 1.2,
 
     _hover: {
       textDecoration: 'none',
+      bgColor: colorScheme ? `${colorScheme}.500` : 'gray.100',
+    },
+
+    _active: {
+      bgColor: colorScheme ? `${colorScheme}.600` : 'gray.200',
     },
   }
-  if (colorScheme === 'primary') {
-    return {
-      ...commonStyles,
-      bgColor: `primary.500`,
-
-      _hover: {
-        ...commonStyles._hover,
-        bgColor: `primary.600`,
-      },
-
-      _active: {
-        bgColor: `primary.700`,
-      },
-    }
-  }
-  return commonStyles
 })
 
 export const Link = defineStyleConfig({
-  variants: { button, primary },
+  variants: { primary, rounded, 'rounded-ghost': roundedGhost },
 })
