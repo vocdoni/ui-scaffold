0e66c0de49afcd6221ed8f0cc41117129b015157
Gerard <g.roura.v@protonmail.com>
2023-09-04 17:21:48 +0200

    alert, buttons and links style changes

--- src/theme/components/Alert.ts
+++ src/theme/components/Alert.ts
@@ -6,11 +6,17 @@ const { defineMultiStyleConfig, definePartsStyle } = createMultiStyleConfigHelpe
 const baseStyle = definePartsStyle((props) => {
   const { status } = props
 
+  const commonStyles = {
+    borderRadius: 'xl',
+    border: '1px solid',
+    w: 'fit-content',
+    mx: 'auto',
+  }
+
   if (status === 'info')
     return {
       container: {
-        borderRadius: 'md',
-        border: '1px solid',
+        ...commonStyles,
         color: 'process_create.alert_info.color',
         bgColor: 'process_create.alert_info.bg',
 
@@ -20,15 +26,11 @@ const baseStyle = definePartsStyle((props) => {
       },
     }
 
-  if (status === 'error')
-    return {
-      container: {
-        borderRadius: 30,
-        w: 'fit-content',
-        mx: 'auto',
-      },
-    }
-  return {}
+  return {
+    container: {
+      ...commonStyles,
+    },
+  }
 })
 
 export const Alert = defineMultiStyleConfig({
