c71f06bec221f695f8a1ccb0a1125819984f4a03
gerouvi <78085338+gerouvi@users.noreply.github.com>
2023-10-16 12:24:19 +0200

    Minor removed logo text and translation changes (#334)
    Co-authored-by: Òscar Casajuana <elboletaire@gmail.com>

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,4 +1,4 @@
-import { Flex, Img, Text } from '@chakra-ui/react'
+import { Flex, Img } from '@chakra-ui/react'
 import { NavLink } from 'react-router-dom'
 import icon from '/assets/vocdoni_icon.png'
 
@@ -6,17 +6,6 @@ const Logo = ({ ...props }) => (
   <NavLink to='/'>
     <Flex alignItems='center' gap={2}>
       <Img src={icon} alt='vocdoni icon' maxWidth={10} />
-      <Text
-        display={{ base: 'none', lg: 'flex' }}
-        flexDirection='row'
-        gap={1}
-        fontSize='xs'
-        fontWeight='bold'
-        lineHeight={1.1}
-        {...props}
-      >
-        Public voting protocol
-      </Text>
     </Flex>
   </NavLink>
 )
