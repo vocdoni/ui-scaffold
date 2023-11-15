241aad7f84c552791e44e9af3e6579d82ecb67cd
Òscar Casajuana <elboletaire@gmail.com>
2023-11-15 19:05:54 +0100

    Load .env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,47 @@
 import react from '@vitejs/plugin-react'
 import { execSync } from 'node:child_process'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-let vocdoniEnvironment = process.env.VOCDONI_ENVIRONMENT
-if (!vocdoniEnvironment) {
-  vocdoniEnvironment = 'stg'
-}
-
-const outDir = process.env.BUILD_PATH
-const base = process.env.BASE_URL || '/'
-
-const commit = execSync('git rev-parse --short HEAD').toString()
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base,
-  build: {
-    outDir,
-  },
-  define: {
-    'import.meta.env.VOCDONI_ENVIRONMENT': JSON.stringify(vocdoniEnvironment),
-    'import.meta.env.FAUCET_AMOUNT': JSON.stringify(process.env.FAUCET_AMOUNT || 800),
-  },
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    createHtmlPlugin({
-      minify: {
-        removeComments: false,
-        collapseWhitespace: true,
-      },
-      inject: {
-        data: {
-          commit: commit.trim(),
+export default ({ mode }) => {
+  // load env variables from .env files
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+
+  let vocdoniEnvironment = process.env.VOCDONI_ENVIRONMENT
+  if (!vocdoniEnvironment) {
+    vocdoniEnvironment = 'stg'
+  }
+
+  const outDir = process.env.BUILD_PATH
+  const base = process.env.BASE_URL || '/'
+
+  const commit = execSync('git rev-parse --short HEAD').toString()
+
+  return defineConfig({
+    base,
+    build: {
+      outDir,
+    },
+    define: {
+      'import.meta.env.VOCDONI_ENVIRONMENT': JSON.stringify(vocdoniEnvironment),
+      'import.meta.env.FAUCET_AMOUNT': JSON.stringify(process.env.FAUCET_AMOUNT || 800),
+    },
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      createHtmlPlugin({
+        minify: {
+          removeComments: false,
+          collapseWhitespace: true,
         },
-      },
-    }),
-  ],
-})
+        inject: {
+          data: {
+            commit: commit.trim(),
+          },
+        },
+      }),
+    ],
+  })
+}
