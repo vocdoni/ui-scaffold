30533747a218fad55c9adf350d2796f124c13637
Aleix <nigeon@gmail.com>
2023-11-15 16:04:39 +0100

    Added FB and Ggoogle oauth providers (#394)
    * Moving params to base64 encoded "state"
    Google and Facebook faucet providers buttons
    
    * + Changed faucet requests.
    + Showing real amounts (coming from faucet)
    
    * Get faucet tokens without connecting the wallet, writing the recipient address
    
    * Requested changes

--- src/components/Faucet/use-faucet.tsx
+++ src/components/Faucet/use-faucet.tsx
@@ -1,7 +1,32 @@
 import { useClient } from '@vocdoni/react-providers'
 
+export type signinUrlParams = {
+  param: string
+  value: any
+}
+
+export type authTypes = {
+  oauth?: number
+  open?: number
+  aragondao?: number
+}
+
+export type oAuthSignInURLFunction = (
+  provider: string,
+  recipient: string,
+  redirectURLParams?: signinUrlParams[]
+) => Promise<string>
+
+export type faucetReceiptFunction = (
+  provider: string,
+  code: string,
+  recipient: string
+) => Promise<{ amount: string; faucetPackage: string }>
+
+export type getAuthTypesFunction = () => Promise<authTypes>
+
 export const useFaucet = () => {
-  const { connected, signer, client } = useClient()
+  const { client } = useClient()
 
   // TODO: Remove this when the client is updated
   let {
@@ -9,25 +34,21 @@ export const useFaucet = () => {
   } = client
   url = url.replace(/v2.*/, 'v2')
 
-  const oAuthSignInURL = async (
-    provider: string,
-    redirectURLParams?: { param: string; value: any }[]
-  ): Promise<string> => {
-    if (!connected) throw new Error('Wallet not connected')
-
+  const oAuthSignInURL: oAuthSignInURLFunction = async (provider, recipient, redirectURLParams?) => {
     const redirectURL = new URL(window.location.href)
-    redirectURL.searchParams.append('provider', provider)
-    redirectURL.searchParams.append('recipient', await signer.getAddress())
-    if (redirectURLParams) {
-      for (const p of redirectURLParams) {
-        redirectURL.searchParams.append(p.param, p.value)
-      }
-    }
 
-    const response = await fetch(`${url}/oauth/authUrl/${provider}`, {
+    const stateParams = [
+      { param: 'provider', value: provider },
+      { param: 'recipient', value: recipient },
+      ...(redirectURLParams || []),
+    ]
+
+    const response = await fetch(`${url}/oauth/authUrl`, {
       method: 'POST',
       body: JSON.stringify({
+        provider,
         redirectURL: redirectURL.toString(),
+        state: btoa(JSON.stringify(stateParams)),
       }),
     })
 
@@ -36,19 +57,33 @@ export const useFaucet = () => {
     return res.url
   }
 
-  const faucetReceipt = async (
-    provider: string,
-    code: string,
-    recipient: string
-  ): Promise<{ amount: string; faucetPackage: string }> => {
-    const response = await fetch(`${url}/oauth/claim/${provider}/${code}/${recipient}`)
+  const faucetReceipt: faucetReceiptFunction = async (provider, code, recipient) => {
+    const redirectURL = new URL(window.location.href)
+    redirectURL.search = ''
+    const response = await fetch(`${url}/oauth/claim`, {
+      method: 'POST',
+      body: JSON.stringify({
+        provider,
+        code: encodeURIComponent(code),
+        recipient,
+        redirectURL: redirectURL.toString(),
+      }),
+    })
     const res = await response.json()
     if (res.error) throw new Error(res.error)
     return res
   }
 
+  const getAuthTypes = async (): Promise<authTypes> => {
+    const response = await fetch(`${url}/authTypes`)
+    const res = await response.json()
+    if (res.error) throw new Error(res.error)
+    return res.auth
+  }
+
   return {
     oAuthSignInURL,
     faucetReceipt,
+    getAuthTypes,
   }
 }
