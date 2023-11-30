06a09e9a991f80831df7ad7eb3bae2756dd72322
Òscar Casajuana <elboletaire@gmail.com>
2023-11-30 13:12:36 +0100

    Faucet waiting period on process create

--- src/components/ProcessCreate/CostPreview.tsx
+++ src/components/ProcessCreate/CostPreview.tsx
@@ -212,13 +212,20 @@ const GetVocTokens = ({ loading, handleSignIn }: { loading: boolean; handleSignI
   const { account } = useClient()
   const { getAuthTypes } = useFaucet()
   const [faucetAmount, setFaucetAmount] = useState<number>(0)
+  const [waitHours, setWaitHours] = useState<number>(0)
 
   useEffect(() => {
     ;(async () => {
       try {
         const atypes = await getAuthTypes()
-        setFaucetAmount(atypes.oauth as number)
-      } catch (e) {}
+        if (atypes.auth.oauth) {
+          setFaucetAmount(atypes.auth.oauth)
+        }
+        setWaitHours(Math.floor(atypes.waitSeconds / 3600))
+      } catch (e) {
+        setFaucetAmount(NaN)
+        setWaitHours(NaN)
+      }
     })()
   }, [])
 
@@ -277,6 +284,7 @@ const GetVocTokens = ({ loading, handleSignIn }: { loading: boolean; handleSignI
               i18nKey='get_voc_tokens.authentification_method_helper'
               values={{
                 faucetAmount,
+                waitHours,
               }}
             />
           </Text>
