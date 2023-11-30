8603386e69cf3780a2c515d1ab452a0b85a232aa
Òscar Casajuana <elboletaire@gmail.com>
2023-11-30 13:12:36 +0100

    Faucet waiting period on process create

--- src/elements/Faucet.tsx
+++ src/elements/Faucet.tsx
@@ -3,17 +3,27 @@ import { useClient } from '@vocdoni/react-providers'
 import { useEffect, useState } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import { Claim } from '~components/Faucet/Claim'
-import { authTypes, useFaucet } from '~components/Faucet/use-faucet'
+import { useFaucet } from '~components/Faucet/use-faucet'
 
 const Faucet = () => {
   const { t } = useTranslation()
   const { account } = useClient()
   const { getAuthTypes } = useFaucet()
+  const [faucetAmount, setFaucetAmount] = useState<number>(0)
+  const [waitHours, setWaitHours] = useState<number>(0)
 
-  const [authTypes, setAuthTypes] = useState<authTypes>({ auth: { oauth: 0, open: 0, aragondao: 0 }, waitSeconds: 0 })
   useEffect(() => {
     ;(async () => {
-      setAuthTypes(await getAuthTypes())
+      try {
+        const atypes = await getAuthTypes()
+        if (atypes.auth.oauth) {
+          setFaucetAmount(atypes.auth.oauth)
+        }
+        setWaitHours(Math.floor(atypes.waitSeconds / 3600))
+      } catch (e) {
+        setFaucetAmount(NaN)
+        setWaitHours(NaN)
+      }
     })()
   }, [])
 
@@ -57,8 +67,8 @@ const Faucet = () => {
               </Heading>
               <Text variant='p'>
                 {t('faucet.general_information.description', {
-                  amount: authTypes.auth.oauth,
-                  waitHours: Math.floor(authTypes.waitSeconds / 3600),
+                  amount: faucetAmount,
+                  waitHours,
                 })}
               </Text>
               <Text variant='p'>{t('faucet.general_information.description2')}</Text>
