880e24f2a02e0b5680aae9f4a391fcb8823c6d4d
Òscar Casajuana <elboletaire@gmail.com>
2023-12-20 17:41:18 +0100

    Properly check for existing accounts on "Checks" ProcessCreate step

--- src/components/Account/use-account-health-tools.tsx
+++ src/components/Account/use-account-health-tools.tsx
@@ -1,9 +1,17 @@
 import { useClient } from '@vocdoni/react-providers'
+import { AccountData, ArchivedAccountData } from '@vocdoni/sdk'
+
+const isRealAccount = (account: AccountData | ArchivedAccountData): account is AccountData => {
+  return (account as AccountData).nonce !== undefined
+}
 
 export const useAccountHealthTools = () => {
   const { account, balance } = useClient()
 
-  const exists = typeof account !== 'undefined'
+  const exists =
+    typeof account !== 'undefined' ||
+    (account && !isRealAccount(account)) ||
+    (account && (account as AccountData).account.name.length > 0)
   const isHealthy = exists && balance > 0
   const existsVariation = (existant: any, notExistant: any) => (exists ? existant : notExistant)
   const healthVariation = (healthy: any, unhealthy: any) => (isHealthy ? healthy : unhealthy)
