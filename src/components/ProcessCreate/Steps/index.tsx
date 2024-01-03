52683c217bef98011a99492ea56cc063f5a3e058
Gerard <g.roura.v@protonmail.com>
2024-01-03 15:15:48 +0100

    fixed issue minw steps form

--- src/components/ProcessCreate/Steps/index.tsx
+++ src/components/ProcessCreate/Steps/index.tsx
@@ -57,7 +57,7 @@ const Steps = () => {
           </Step>
         ))}
       </Stepper>
-      <Box>
+      <Box w='full'>
         <StepsForm
           steps={steps}
           activeStep={activeStep}
