b6262df1aba8c659b1d51c5ab87184f251f1eadc
Òscar Casajuana <elboletaire@underave.net>
2024-01-16 17:09:49 +0100

    Upgrade SDK to 0.7.1 (#469)
    * Force SDK 0.7.1 (packed version)
    
    * Properly check for existing accounts on "Checks" ProcessCreate step
    
    * Fix incorrect condition
    
    * Rename Account => Organization in health tools hook
    
    * Upgrade vocdoni-sdk to latest main version
    
    * Upgrade vocdoni sdk with latest version
    
    * Upgrade to sdk stable 0.7.1
    
    * Upgrade chakra components dependencies (to properly match SDK 0.7.1)
    
    * Upgrade react-providers

--- src/components/Account/use-account-health-tools.tsx
+++ src/components/Account/use-account-health-tools.tsx
@@ -1,9 +1,14 @@
 import { useClient } from '@vocdoni/react-providers'
+import { AccountData, ArchivedAccountData } from '@vocdoni/sdk'
 
-export const useAccountHealthTools = () => {
+const isRealAccount = (account: AccountData | ArchivedAccountData): account is AccountData => {
+  return (account as AccountData).nonce !== undefined
+}
+
+export const useOrganizationHealthTools = () => {
   const { account, balance } = useClient()
 
-  const exists = typeof account !== 'undefined'
+  const exists = typeof account !== 'undefined' && isRealAccount(account) && account.account.name.default.length > 0
   const isHealthy = exists && balance > 0
   const existsVariation = (existant: any, notExistant: any) => (exists ? existant : notExistant)
   const healthVariation = (healthy: any, unhealthy: any) => (isHealthy ? healthy : unhealthy)
