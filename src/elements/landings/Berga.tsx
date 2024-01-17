598ff0a06659397f09aad39471f590170f046425
jpa <jaulet@aragonlabs.com>
2024-01-17 10:13:51 +0100

    Last improvements in the layout and boxes

--- src/elements/landings/Berga.tsx
+++ src/elements/landings/Berga.tsx
@@ -2,13 +2,15 @@ import { Box, Flex, Image, Link, Text } from '@chakra-ui/react'
 import { Link as ReactRouterLink } from 'react-router-dom'
 import bergaImg from '/assets/berga.jpg'
 import vocdoniIcon from '/assets/vocdoni.png'
+import juvenilImg from '/assets/juvenil.jpg'
+import adultImg from '/assets/adults.jpg'
 
 const Berga = () => {
   return (
     <Flex flexDirection='column' gap={10} maxW='900px' mx='auto' p={5} minH='100vh'>
       <Image src={bergaImg} width='300px' alt='ajuntament berga escut' mx='auto' />
       <Box>
-        <Text as='h1' fontWeight='bold' fontSize='24px' textAlign='center'>
+        <Text as='h1' fontWeight='bold' fontSize='36px' textAlign='center'>
           Pressupostos Participatius 2024
         </Text>
         <Text as='h2' fontSize='16px' textAlign='center'>
@@ -16,45 +18,55 @@ const Berga = () => {
         </Text>
       </Box>
       <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
+        <Text as='h3' fontSize='12px' fontWeight='bold'>Pressupost Participatiu de Berga 2024</Text>
+        <Text>El pressupost participatiu és una eina de participació ciutadana per a què els veïns i les veïnes puguin decidir a quins projectes o activitats es destina una part del pressupost públic municipal.
+              <br />
+              L'Ajuntament de Berga promou per cinquè any consecutiu el pressupost participatiu. Comptarà amb una partida de 30.000€, els quals es podran destinar tant a propostes d'inversió (màxim de 20.000 €) com de programació cultural (màxim de 10.000€).
+        </Text>
+      </Text>
+      <Text>
+        <Text as='h3' fontSize='12px' fontWeight='bold'>Pressupost Participatiu Juvenil</Text>
+        <Text>El consistori promou per tercer any el pressupost participatiu adreçat a joves de 12 a 15 anys que s'organitzarà a través dels centres educatius de la ciutat. Comptarà amb una partida de 5.000€ que es podran destinar tant a propostes d'inversió com de programació cultural.<br />
+              En la segona edició (2023) va resultar guanyadora la realització d'una vetllada de concerts per a l'alumnat dels centres educatius de Berga, que es va dur a terme el dissabte, 16 de setembre.
+        </Text>
       </Text>
       <Box>
-        <Text fontWeight='bold' alignSelf='start' mb={5}>
-          Seleccioneu una opció depenent de la teva edat
+        <Text alignSelf='start' mb={10}>
+          <strong>Seleccioneu</strong> una opció depenent de la teva edat
         </Text>
-        <Flex gap={5} flexDirection={{ base: 'column', lg: 'row' }}>
+        <Flex gap={5} flexDirection={{ base: 'row', sm: 'column', md: 'row' }}>
           <Link
             as={ReactRouterLink}
             flexGrow={1}
+            flexBasis='0%'
             color='black'
             textDecoration='none'
             textAlign='center'
             fontWeight='bold'
           >
-            <Box h={{ base: '50px', lg: '200px' }} boxShadow='0px 0px 10px 2px lightgray' borderRadius='md' mb={5} />
-            <Text>Juvenil</Text>
-            <Text>14 - 18 anys</Text>
+            <Box h={{ base: '200px' }} boxShadow='0px 0px 10px 2px lightgray' borderRadius='md' mb={50} p={20}>
+              <Text fontSize='18px'>Accedeix a la votació Juvenil</Text>
+              <Text>12 - 15 anys</Text>
+            </Box>
           </Link>
           <Link
             as={ReactRouterLink}
             flexGrow={1}
+            flexBasis='0%'
             color='black'
             textDecoration='none'
             textAlign='center'
             fontWeight='bold'
           >
-            <Box h={{ base: '50px', lg: '200px' }} boxShadow='0px 0px 10px 2px lightgray' borderRadius='md' mb={5} />
-            <Text>General</Text>
-            <Text>+18 anys</Text>
+            <Box h={{ base: '200px' }} boxShadow='0px 0px 10px 2px lightgray' borderRadius='md' mb={50} py={20}>
+              <Text fontSize='18px'>Accedeix a la votació General</Text>
+              <Text>+16 anys</Text>
+            </Box>
           </Link>
         </Flex>
       </Box>
       <Text>
-        Es demanarà el teu DNI + data de naixament un cop seleccionada una opció de pressupostos participatius
+        Per poder accedir a la votació, us demanarem el vostre número de DNI (Document Nacional d'Identitat) i data de naixement. Posteriorment, podreu seleccionar les vostres opcions i enviar el vot de forma segura.
       </Text>
       <Flex justifyContent='center' mt='auto'>
         <Image src={vocdoniIcon} width='200px' alt='vocdoni logo' />
