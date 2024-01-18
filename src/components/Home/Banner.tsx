5d68912b2740491f8c56e7303be033fcbc80732d
jp_aulet <jpaulet@users.noreply.github.com>
2024-01-18 11:19:29 +0100

    Theme fixes and new vocdoni landing (#490)
    Note most of these changes wont' be visible until we revert the current forced landing.
    ---------
    
    Co-authored-by: Gerard <g.roura.v@protonmail.com>

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -10,44 +10,41 @@ const Banner = () => {
 
   return (
     <Flex
-      flexDirection={{ base: 'column', md: 'row' }}
+      flexDirection={{ base: 'column' }}
       justifyContent='space-between'
       alignItems='center'
-      maxW={250}
+      maxW='1250px'
       mx='auto'
-      pl={{ md: 8 }}
-      mb={20}
+      mb={50}
+      pt={20}
     >
-      <Img
-        src={bannerLogo}
-        alt=''
-        width={{ base: '50%', sm: '40%', md: '300px', md2: '320px', lg: '350px', xl: '400px' }}
-        order={{ base: 0, md: 1 }}
-      />
+      <Img src={bannerLogo} alt='' width={{ base: '100%', md: '80%', lg: '60%' }} order={0} mb={10} />
 
-      <Box pt={{ base: 5 }} px={{ sm: 10, md: 0 }}>
+      <Box pb={10} px={{ base: '32px', xl: 0 }}>
         <Text
           as='h1'
-          fontSize={{ base: 'xl3', sm: 'xl4', md: 'xl3' }}
+          fontSize={{ base: 'xl5' }}
           fontWeight='extrabold'
-          textAlign={{ base: 'center', md: 'start' }}
-          pb={{ base: 3, md: 1 }}
-          width={{ md: '420px' }}
-          lineHeight={{ md: 1.4 }}
+          textAlign={{ base: 'center', lg: 'start' }}
+          pb={{ base: 3, lg: 1 }}
+          lineHeight={{ base: 1.3, lg: 1.4 }}
+          fontFamily='"Archivo", sans-serif'
         >
           {t('banner.title')}
         </Text>
-        <Text as='h2' fontSize={{ base: 'xl', md: 'lg' }} textAlign={{ base: 'center', md: 'start' }}>
+        <Text
+          as='h2'
+          fontSize={{ base: 'xl2' }}
+          fontFamily='"Archivo", sans-serif'
+          textAlign={{ base: 'center', lg: 'start' }}
+          marginBottom={{ base: 10, lg: 4 }}
+        >
           {t('banner.subtitle')}
+          <br />
+          {t('banner.subtitle1')}
         </Text>
         {isConnected && (
-          <Flex
-            flexDirection={{ base: 'column', sm: 'row' }}
-            gap={5}
-            justifyContent={{ base: 'center', md: 'start' }}
-            alignItems='center'
-            marginTop={{ base: 10, md: 4 }}
-          >
+          <Flex gap={5} justifyContent={{ base: 'center', lg: 'start' }} alignItems='center'>
             <Button
               as={ReactRouterLink}
               to='/processes/create'
