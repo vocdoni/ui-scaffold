bb00e7505f18c8cb1f29d9ded0e9582ce5776614
Òscar Casajuana <elboletaire@gmail.com>
2024-01-31 16:03:01 +0100

    Check for existing accounts after login
    If the account does not exist, it'll be created with empty data (which
    will generate the SIK for the first time).
    
    closes #528

--- src/components/Account/use-account-health-tools.tsx
+++ src/components/Account/use-account-health-tools.tsx
@@ -1,14 +1,16 @@
 import { useClient } from '@vocdoni/react-providers'
 import { AccountData, ArchivedAccountData } from '@vocdoni/sdk'
+import { useEffect, useState } from 'react'
+import { useAccount } from 'wagmi'
 
-const isRealAccount = (account: AccountData | ArchivedAccountData): account is AccountData => {
+const isSignerAccount = (account: AccountData | ArchivedAccountData): account is AccountData => {
   return (account as AccountData).nonce !== undefined
 }
 
 export const useOrganizationHealthTools = () => {
   const { account, balance } = useClient()
 
-  const exists = typeof account !== 'undefined' && isRealAccount(account) && account.account.name.default.length > 0
+  const exists = typeof account !== 'undefined' && isSignerAccount(account) && account.account.name.default.length > 0
   const isHealthy = exists && balance > 0
   const existsVariation = (existant: any, notExistant: any) => (exists ? existant : notExistant)
   const healthVariation = (healthy: any, unhealthy: any) => (isHealthy ? healthy : unhealthy)
@@ -19,4 +21,35 @@ export const useOrganizationHealthTools = () => {
     healthVariation,
     isHealthy,
   }
+}
+
+export const useAccountHealthTools = () => {
+  const { account } = useClient()
+
+  const exists = typeof account !== 'undefined' && isSignerAccount(account)
+
+  return {
+    exists,
+  }
+}
+
+/**
+ * Creates an account if it is connected and does not exist yet.
+ */
+export const useAccountCreator = () => {
+  const { isConnected } = useAccount()
+  const { client, account, createAccount } = useClient()
+
+  const { exists } = useAccountHealthTools()
+  const [creating, setCreating] = useState(false)
+
+  useEffect(() => {
+    if (!isConnected || (isConnected && exists) || creating || !client.wallet) return
+    ;(async () => {
+      setCreating(true)
+      await createAccount()
+      setCreating(false)
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isConnected, exists, account, client.wallet, creating])
 }
