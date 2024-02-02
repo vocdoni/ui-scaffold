5dd1acb3ddfb09b5c6074a1e16d82bf0da9c5a90
Òscar Casajuana <elboletaire@gmail.com>
2024-02-02 12:27:08 +0100

    Remove unused editor stuff

--- src/components/Editor/plugins/ToolbarPlugin.tsx
+++ src/components/Editor/plugins/ToolbarPlugin.tsx
@@ -94,29 +94,6 @@ function getCodeLanguageOptions(): [string, string][] {
 
 const CODE_LANGUAGE_OPTIONS = getCodeLanguageOptions()
 
-const FONT_FAMILY_OPTIONS: [string, string][] = [
-  ['Arial', 'Arial'],
-  ['Courier New', 'Courier New'],
-  ['Georgia', 'Georgia'],
-  ['Times New Roman', 'Times New Roman'],
-  ['Trebuchet MS', 'Trebuchet MS'],
-  ['Verdana', 'Verdana'],
-]
-
-const FONT_SIZE_OPTIONS: [string, string][] = [
-  ['10px', '10px'],
-  ['11px', '11px'],
-  ['12px', '12px'],
-  ['13px', '13px'],
-  ['14px', '14px'],
-  ['15px', '15px'],
-  ['16px', '16px'],
-  ['17px', '17px'],
-  ['18px', '18px'],
-  ['19px', '19px'],
-  ['20px', '20px'],
-]
-
 const ELEMENT_FORMAT_OPTIONS: {
   [key in Exclude<ElementFormatType, ''>]: {
     icon: string
@@ -257,55 +234,6 @@ function Divider(): JSX.Element {
   return <div className='divider' />
 }
 
-function FontDropDown({
-  editor,
-  value,
-  style,
-  disabled = false,
-}: {
-  editor: LexicalEditor
-  value: string
-  style: string
-  disabled?: boolean
-}): JSX.Element {
-  const handleClick = useCallback(
-    (option: string) => {
-      editor.update(() => {
-        const selection = $getSelection()
-        if (selection !== null) {
-          $patchStyleText(selection, {
-            [style]: option,
-          })
-        }
-      })
-    },
-    [editor, style]
-  )
-
-  const buttonAriaLabel =
-    style === 'font-family' ? 'Formatting options for font family' : 'Formatting options for font size'
-
-  return (
-    <DropDown
-      disabled={disabled}
-      buttonClassName={'toolbar-item ' + style}
-      buttonLabel={value}
-      buttonIconClassName={style === 'font-family' ? 'icon block-type font-family' : ''}
-      buttonAriaLabel={buttonAriaLabel}
-    >
-      {(style === 'font-family' ? FONT_FAMILY_OPTIONS : FONT_SIZE_OPTIONS).map(([option, text]) => (
-        <DropDownItem
-          className={`item ${dropDownActiveClass(value === option)} ${style === 'font-size' ? 'fontsize-item' : ''}`}
-          onClick={() => handleClick(option)}
-          key={option}
-        >
-          <span className='text'>{text}</span>
-        </DropDownItem>
-      ))}
-    </DropDown>
-  )
-}
-
 function ElementFormatDropdown({
   editor,
   value,
@@ -410,9 +338,7 @@ export default function ToolbarPlugin({ setIsLinkEditMode }: { setIsLinkEditMode
   const [blockType, setBlockType] = useState<keyof typeof blockTypeToBlockName>('paragraph')
   const [rootType, setRootType] = useState<keyof typeof rootTypeToRootName>('root')
   const [selectedElementKey, setSelectedElementKey] = useState<NodeKey | null>(null)
-  const [fontColor, setFontColor] = useState<string>('#000')
   const [bgColor, setBgColor] = useState<string>('#fff')
-  const [fontFamily, setFontFamily] = useState<string>('Arial')
   const [elementFormat, setElementFormat] = useState<ElementFormatType>('left')
   const [isLink, setIsLink] = useState(false)
   const [isBold, setIsBold] = useState(false)
@@ -492,9 +418,7 @@ export default function ToolbarPlugin({ setIsLinkEditMode }: { setIsLinkEditMode
         }
       }
       // Handle buttons
-      setFontColor($getSelectionStyleValueForProperty(selection, 'color', '#000'))
       setBgColor($getSelectionStyleValueForProperty(selection, 'background-color', '#fff'))
-      setFontFamily($getSelectionStyleValueForProperty(selection, 'font-family', 'Arial'))
       let matchingParent
       if ($isLinkNode(parent)) {
         // If node is a link, we need to fetch the parent paragraph node to set format
@@ -636,20 +560,6 @@ export default function ToolbarPlugin({ setIsLinkEditMode }: { setIsLinkEditMode
     })
   }, [activeEditor])
 
-  const onFontColorSelect = useCallback(
-    (value: string, skipHistoryStack: boolean) => {
-      applyStyleText({ color: value }, skipHistoryStack)
-    },
-    [applyStyleText]
-  )
-
-  const onBgColorSelect = useCallback(
-    (value: string, skipHistoryStack: boolean) => {
-      applyStyleText({ 'background-color': value }, skipHistoryStack)
-    },
-    [applyStyleText]
-  )
-
   const insertLink = useCallback(() => {
     if (!isLink) {
       setIsLinkEditMode(true)
@@ -709,8 +619,6 @@ export default function ToolbarPlugin({ setIsLinkEditMode }: { setIsLinkEditMode
       )}
 
       <>
-        <FontDropDown disabled={!isEditable} style={'font-family'} value={fontFamily} editor={editor} />
-        <Divider />
         <button
           disabled={!isEditable}
           onClick={() => {
