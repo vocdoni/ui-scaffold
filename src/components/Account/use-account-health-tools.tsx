257fbedf6022b61c6c7307722d98ddfe7d47edca
Òscar Casajuana <elboletaire@gmail.com>
2024-02-07 17:53:09 +0100

    Remove reminiscenses of 294f01c5d14

--- src/components/Account/use-account-health-tools.tsx
+++ src/components/Account/use-account-health-tools.tsx
@@ -1,9 +1,5 @@
-import { Button, ModalBody, Text } from '@chakra-ui/react'
-import { useConfirm } from '@vocdoni/chakra-components'
 import { useClient } from '@vocdoni/react-providers'
 import { AccountData, ArchivedAccountData } from '@vocdoni/sdk'
-import { useCallback, useEffect, useState } from 'react'
-import { useAccount } from 'wagmi'
 
 const isSignerAccount = (account: AccountData | ArchivedAccountData): account is AccountData => {
   return (account as AccountData).nonce !== undefined
@@ -33,80 +29,4 @@ export const useAccountHealthTools = () => {
   return {
     exists,
   }
-}
-
-/**
- * Creates an account if it is connected and does not exist yet.
- */
-export const useAccountCreator = () => {
-  const { isConnected } = useAccount()
-  const { client, account, createAccount } = useClient()
-  const { confirm, isOpen } = useConfirm()
-  const { exists } = useAccountHealthTools()
-  const [creating, setCreating] = useState<boolean>(false)
-
-  console.log('hook rendered')
-
-  useEffect(() => {
-    if (!isConnected || (isConnected && exists) || !client.wallet || isOpen || creating) return
-    ;(async () => {
-      setCreating(true)
-      await confirm(<CreatingAccountModal />)
-      setCreating(false)
-    })()
-    // ;(async () => {
-    //   if (creating) return
-
-    //   setCreating(true)
-    //   await createAccount()
-    //   setCreating(false)
-    // })()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isConnected, exists, account, client.wallet])
-}
-
-const CreatingAccountModal = () => {
-  const { isConnected } = useAccount()
-  const {
-    client,
-    account,
-    createAccount,
-    errors: { create: error },
-  } = useClient()
-  const { exists } = useAccountHealthTools()
-  const [creating, setCreating] = useState<boolean>(false)
-
-  console.log('modal rendered')
-
-  const create = useCallback(async () => {
-    console.log('entra al create')
-    if (creating) return
-
-    setCreating(true)
-    await createAccount()
-    setCreating(false)
-  }, [creating])
-
-  useEffect(() => {
-    console.log("entra a l'efecte")
-    if (!isConnected || (isConnected && exists) || creating || !client.wallet) return
-    ;(async () => {
-      create()
-    })()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isConnected, exists, account, client.wallet])
-
-  return (
-    <ModalBody>
-      <p>Creating...!</p>
-      {error && (
-        <>
-          <Text color='error' textAlign='center' mt={5}>
-            {error}
-          </Text>
-          <Button onClick={create}>Retry</Button>
-        </>
-      )}
-    </ModalBody>
-  )
 }
