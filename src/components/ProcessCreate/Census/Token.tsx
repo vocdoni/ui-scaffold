e1763d81209f9239ab1883960ba6655a2a9d6c7f
selankon <selankon@selankon.xyz>
2024-02-29 16:46:42 +0100

    Disable gitcoin passport tokens

--- src/components/ProcessCreate/Census/Token.tsx
+++ src/components/ProcessCreate/Census/Token.tsx
@@ -36,6 +36,7 @@ export interface FilterOptionOption<Option> {
   readonly value: string
   readonly data: Option
 }
+
 type GrupedTokenTypes = {
   label: string
   options: TokenSummary[] | { name: string; synced: boolean; type: string }[]
@@ -136,6 +137,7 @@ export const CensusTokens = () => {
           maticmum: 4,
         }
         const filteredTag = 'aragon'
+        const filteredType = 'gitcoinpassport'
 
         const defaultOrder: number = 10000
 
@@ -151,12 +153,12 @@ export const CensusTokens = () => {
         })
         setChains(chs)
 
-        const tks = await client.getSupportedTokens()
+        const tks = (await client.getSupportedTokens()).filter((tk) => tk.type !== filteredType)
 
         const tags = [
           ...new Set(
             tks
-              .filter((tk) => tk.tags.length > 0)
+              .filter((tk) => tk.tags.length > 0 && tk.type !== filteredType)
               .map((tk) => tk.tags)
               .flat()
           ),
