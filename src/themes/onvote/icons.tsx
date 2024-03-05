8aafa3dcaeb5d4fc66f7b39e986ed0a94cd06ee2
jpa <jaulet@aragonlabs.com>
2024-03-05 18:41:23 +0100

    Added mobile icon to onvote

--- src/themes/onvote/icons.tsx
+++ src/themes/onvote/icons.tsx
@@ -3,11 +3,14 @@ import check from '/assets/check-icon.svg'
 import close from '/assets/close-icon.svg'
 import goback from '/assets/goback-icon.svg'
 import logo from '/assets/onvote-icon.svg'
+import logoMbl from '/assets/vocdoni_icon.png'
 
 export const Close = (props: ImgProps) => <Img src={close} alt='close' {...props} />
 
 export const Check = (props: ImgProps) => <Img src={check} alt='check' {...props} />
 
 export const Logo = (props: ImgProps) => <Img src={logo} alt='onvote logo' maxWidth={12} {...props} />
+
+export const LogoMbl = (props: ImgProps) => <Img src={logoMbl} alt='vocdoni mbl icon' maxWidth={10} {...props} />
 
 export const GoBack = (props: ImgProps) => <Img src={goback} alt='go back' {...props} />
