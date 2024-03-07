44988b3af6ae7ec046ba781cb3c7298e1ea44d2f
selankon <selankon@selankon.xyz>
2024-03-07 09:55:01 +0100

    Fix first true

--- src/components/ProcessCreate/Steps/use-steps.tsx
+++ src/components/ProcessCreate/Steps/use-steps.tsx
@@ -53,9 +53,9 @@ export const useStepContents = () => {
   const { t } = useTranslation()
   const steps = [
     { title: t('form.process_create.steps.checks'), Contents: Checks },
-    { title: t('form.process_create.steps.info'), Contents: Info },
+    { title: t('form.process_create.steps.info'), Contents: Info, first: true },
     { title: t('form.process_create.steps.questions'), Contents: Questions },
-    { title: t('form.process_create.steps.census'), Contents: Census, first: true },
+    { title: t('form.process_create.steps.census'), Contents: Census },
     { title: t('form.process_create.steps.confirm'), Contents: Confirm },
   ]
 
