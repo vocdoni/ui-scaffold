d5aa16292b7a8d7d9d66169811ff9c5db6fa66b6
selankon <selankon@selankon.xyz>
2024-03-18 09:54:34 +0100

    Give more horizontal space

--- src/components/Home/Voting.tsx
+++ src/components/Home/Voting.tsx
@@ -6,37 +6,35 @@ import { TbDiscountCheckFilled } from 'react-icons/tb'
 const VotingTypesBanner = () => {
   const { t } = useTranslation()
   return (
-    <>
-      <Flex className='voting-type'>
-        <Card variant='types-voting'>
-          <CardBody>
-            <Icon as={TbDiscountCheckFilled} />
-            <Text>{t('banner_voting_types.anonymous_title')}</Text>
-          </CardBody>
-          <CardFooter>
-            <Text>{t('banner_voting_types.anonymous_description')}</Text>
-          </CardFooter>
-        </Card>
-        <Card variant='types-voting'>
-          <CardBody>
-            <Icon as={BsStars} />
-            <Text>{t('banner_voting_types.token_title')}</Text>
-          </CardBody>
-          <CardFooter>
-            <Text> {t('banner_voting_types.token_description')}</Text>
-          </CardFooter>
-        </Card>
-        <Card variant='types-voting'>
-          <CardBody>
-            <Icon as={BsArrowUpRight} />
-            <Text>{t('banner_voting_types.flexible_title')}</Text>
-          </CardBody>
-          <CardFooter>
-            <Text>{t('banner_voting_types.flexible_description')}</Text>
-          </CardFooter>
-        </Card>
-      </Flex>
-    </>
+    <Flex className='voting-type' maxW={'1250px'}>
+      <Card variant='types-voting'>
+        <CardBody>
+          <Icon as={TbDiscountCheckFilled} />
+          <Text>{t('banner_voting_types.anonymous_title')}</Text>
+        </CardBody>
+        <CardFooter>
+          <Text>{t('banner_voting_types.anonymous_description')}</Text>
+        </CardFooter>
+      </Card>
+      <Card variant='types-voting'>
+        <CardBody>
+          <Icon as={BsStars} />
+          <Text>{t('banner_voting_types.token_title')}</Text>
+        </CardBody>
+        <CardFooter>
+          <Text> {t('banner_voting_types.token_description')}</Text>
+        </CardFooter>
+      </Card>
+      <Card variant='types-voting'>
+        <CardBody>
+          <Icon as={BsArrowUpRight} />
+          <Text>{t('banner_voting_types.flexible_title')}</Text>
+        </CardBody>
+        <CardFooter>
+          <Text>{t('banner_voting_types.flexible_description')}</Text>
+        </CardFooter>
+      </Card>
+    </Flex>
   )
 }
 export default VotingTypesBanner
