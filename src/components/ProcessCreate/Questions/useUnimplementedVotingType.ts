929ba9908c55adb186cc024223fca34161a8b2b1
selankon <selankon@selankon.xyz>
2024-05-15 10:54:46 +0200

    Add approval to supported voting types

--- src/components/ProcessCreate/Questions/useUnimplementedVotingType.ts
+++ src/components/ProcessCreate/Questions/useUnimplementedVotingType.ts
@@ -1,24 +1,20 @@
 import { useTranslation } from 'react-i18next'
 
 import { GoNumber } from 'react-icons/go'
-import { HiCheckBadge } from 'react-icons/hi2'
 import { ImListNumbered } from 'react-icons/im'
 import { MdOutlineLibraryAddCheck } from 'react-icons/md'
 
 export const UnimplementedVotingTypeMulti = 'multi'
-export const UnimplementedVotingTypeApproval = 'approval'
 export const UnimplementedVotingTypeParticipatory = 'participatory'
 export const UnimplementedVotingTypeBorda = 'borda'
 
 export type UnimplementedVotingType =
   | typeof UnimplementedVotingTypeMulti
-  | typeof UnimplementedVotingTypeApproval
   | typeof UnimplementedVotingTypeParticipatory
   | typeof UnimplementedVotingTypeBorda
 
 export const UnimplementedVotingType = [
   UnimplementedVotingTypeMulti as UnimplementedVotingType,
-  UnimplementedVotingTypeApproval as UnimplementedVotingType,
   UnimplementedVotingTypeParticipatory as UnimplementedVotingType,
   UnimplementedVotingTypeBorda as UnimplementedVotingType,
 ]
@@ -33,11 +29,6 @@ export const useUnimplementedVotingType = () => {
         title: t('process_create.question.multi_choice.title'),
         description: t('process_create.question.multi_choice.description'),
         icon: MdOutlineLibraryAddCheck,
-      },
-      [UnimplementedVotingTypeApproval]: {
-        title: t('process_create.question.approval_voting.title'),
-        description: t('process_create.question.approval_voting.description'),
-        icon: HiCheckBadge,
       },
       [UnimplementedVotingTypeParticipatory]: {
         title: t('process_create.question.participation_budgeting.title'),
--- src/components/ProcessCreate/Questions/useVotingType.ts
+++ src/components/ProcessCreate/Questions/useVotingType.ts
@@ -2,10 +2,11 @@ import { useTranslation } from 'react-i18next'
 import { GiChoice } from 'react-icons/gi'
 
 export const VotingTypeSingle = 'single'
+export const UnimplementedVotingTypeApproval = 'approval'
 
-export type VotingType = typeof VotingTypeSingle
+export type VotingType = typeof VotingTypeSingle | typeof UnimplementedVotingTypeApproval
 
-export const VotingTypes = [VotingTypeSingle as VotingType]
+export const VotingTypes = [VotingTypeSingle as VotingType, UnimplementedVotingTypeApproval as VotingType]
 
 export const useVotingType = () => {
   const { t } = useTranslation()
@@ -16,6 +17,11 @@ export const useVotingType = () => {
       [VotingTypeSingle]: {
         title: t('process_create.question.single_choice.title'),
         description: t('process_create.question.single_choice.description'),
+        icon: GiChoice,
+      },
+      [UnimplementedVotingTypeApproval]: {
+        title: t('process_create.question.approval_voting.title'),
+        description: t('process_create.question.approval_voting.description'),
         icon: GiChoice,
       },
     },
