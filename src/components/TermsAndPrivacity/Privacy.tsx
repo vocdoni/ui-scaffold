ceceef6b2ea67ae2037fb9779e9f58cd355b2e4e
Gerard <g.roura.v@protonmail.com>
2024-05-15 16:10:15 +0200

    terms and privacy styles

--- src/components/TermsAndPrivacity/Privacy.tsx
+++ src/components/TermsAndPrivacity/Privacy.tsx
@@ -1,74 +1,105 @@
-import { Box, Container, Heading, Text, Link, List, ListItem } from '@chakra-ui/react'
+import { Box, Flex, Heading, Link, List, ListItem, Text } from '@chakra-ui/react'
 
 const Privacy = () => {
   return (
-    <Box>
+    <Flex flexDirection='column' gap={5} className='site-wrapper' mb={44} mt={6} mx='auto'>
       <Box mb={5}>
-        <Heading as="h1" size="xl" mb={3}>
+        <Heading as='h1' size='xl' mb={3}>
           Privacy Policy
         </Heading>
-        <Text>This document outlines the privacy practices for Vocdoni Association ("Vocdoni") concerning the use of its websites and services.</Text>
+        <Text>
+          This document outlines the privacy practices for Vocdoni Association ("Vocdoni") concerning the use of its
+          websites and services.
+        </Text>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           1. Information Collection
         </Heading>
         <List spacing={2}>
           <ListItem>
-            <Text><strong>1.1</strong> Vocdoni may collect user personal data such as name, organization name, size, type and email addresses when provided as the organization's contact email, though using personal email addresses for this purpose is not recommended.</Text>
+            <Text>
+              <strong>1.1</strong> Vocdoni may collect user personal data such as name, organization name, size, type
+              and email addresses when provided as the organization's contact email, though using personal email
+              addresses for this purpose is not recommended.
+            </Text>
           </ListItem>
           <ListItem>
-            <Text><strong>1.2</strong> User consent may be required for the use of certain personal data, which can be withdrawn at any time using Vocdoni's contact details provided below.</Text>
+            <Text>
+              <strong>1.2</strong> User consent may be required for the use of certain personal data, which can be
+              withdrawn at any time using Vocdoni's contact details provided below.
+            </Text>
           </ListItem>
           <ListItem>
-            <Text><strong>1.3</strong> Additional personal information may be collected and processed as necessary to deliver services, fulfill contractual obligations, or comply with legal requirements.</Text>
+            <Text>
+              <strong>1.3</strong> Additional personal information may be collected and processed as necessary to
+              deliver services, fulfill contractual obligations, or comply with legal requirements.
+            </Text>
           </ListItem>
         </List>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           2. Method of Collection
         </Heading>
-        <Text>Vocdoni processes personal information provided directly by users through various means, including registration on any Vocdoni website, service procurement, or communications.</Text>
+        <Text>
+          Vocdoni processes personal information provided directly by users through various means, including
+          registration on any Vocdoni website, service procurement, or communications.
+        </Text>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           3. Use of Information
         </Heading>
-        <Text>The collected information is used to provide and manage services, conduct marketing and business activities, and support customer service and marketing efforts.</Text>
+        <Text>
+          The collected information is used to provide and manage services, conduct marketing and business activities,
+          and support customer service and marketing efforts.
+        </Text>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           4. Information Sharing
         </Heading>
-        <Text>Vocdoni may share personal information with other companies within the Vocdoni Association or third-party service providers assisting with our services.</Text>
+        <Text>
+          Vocdoni may share personal information with other companies within the Vocdoni Association or third-party
+          service providers assisting with our services.
+        </Text>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           5. Information Security
         </Heading>
         <Text>Vocdoni commits to securing collected personal information with appropriate measures.</Text>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           6. Rights of Users
         </Heading>
-        <Text>Users have rights to access, correct, delete, and restrict the processing of their data, among other rights outlined herein.</Text>
+        <Text>
+          Users have rights to access, correct, delete, and restrict the processing of their data, among other rights
+          outlined herein.
+        </Text>
       </Box>
 
       <Box mb={5}>
-        <Heading as="h2" size="lg" mb={2}>
+        <Heading as='h2' size='lg' mb={2}>
           7. Contacting Vocdoni
         </Heading>
-        <Text>For further details on our data practices or to exercise your rights, please contact us at <Link href="mailto:privacy@vocdoni.org" color="teal.500">privacy@vocdoni.org</Link>.</Text>
+        <Text>
+          For further details on our data practices or to exercise your rights, please contact us at{' '}
+          <Link href='mailto:privacy@vocdoni.org' color='teal.500'>
+            privacy@vocdoni.org
+          </Link>
+          .
+        </Text>
       </Box>
-    </Box>
+    </Flex>
   )
 }
 
--- src/components/TermsAndPrivacity/Terms.tsx
+++ src/components/TermsAndPrivacity/Terms.tsx
@@ -1,71 +1,102 @@
-import { Box, Heading, Text, ListItem, UnorderedList } from '@chakra-ui/react'
+import { Flex, Heading, ListItem, Text, UnorderedList } from '@chakra-ui/react'
 
 const Terms = () => {
   return (
-    <Box>
-      <Heading as="h1" size="xl">Terms and Conditions for Vocdoni Association</Heading>
+    <Flex flexDirection='column' gap={5} className='site-wrapper' mb={44} mt={6} mx='auto'>
+      <Heading as='h1' size='xl'>
+        Terms and Conditions for Vocdoni Association
+      </Heading>
 
-      <Heading as="h2" size="lg">1. General Information</Heading>
-      <Text as="p">
-        The Vocdoni Association, headquartered in Switzerland, operates the app.vocdoni.io and onvote.app platforms.
-        Our mission is to provide secure and private digital voting solutions through a distributed architecture.
+      <Heading as='h2' size='lg'>
+        1. General Information
+      </Heading>
+      <Text>
+        The Vocdoni Association, headquartered in Switzerland, operates the app.vocdoni.io and onvote.app platforms. Our
+        mission is to provide secure and private digital voting solutions through a distributed architecture.
       </Text>
-      <Text as="p">
-        These platforms prioritize user privacy and data security, ensuring compliance with the European Union's General Data Protection Regulation (GDPR).
+      <Text>
+        These platforms prioritize user privacy and data security, ensuring compliance with the European Union's General
+        Data Protection Regulation (GDPR).
       </Text>
 
-      <Heading as="h2" size="lg">2. Data Collection and Storage</Heading>
-      <Text as="p">We emphasize user data security. Here is an overview:</Text>
+      <Heading as='h2' size='lg'>
+        2. Data Collection and Storage
+      </Heading>
+      <Text>We emphasize user data security. Here is an overview:</Text>
       <UnorderedList>
         <ListItem>
-          <strong>Information Collected:</strong> We collect and process personal data necessary for service provision. This includes data provided by voting organizers and voters to verify identities, manage voting events, and facilitate the overall voting process.
+          <strong>Information Collected:</strong> We collect and process personal data necessary for service provision.
+          This includes data provided by voting organizers and voters to verify identities, manage voting events, and
+          facilitate the overall voting process.
         </ListItem>
         <ListItem>
-          <strong>Encrypted Storage:</strong> All personal data is stored in encrypted form, ensuring that no personal data is stored in plain text or accessible without proper authorization.
+          <strong>Encrypted Storage:</strong> All personal data is stored in encrypted form, ensuring that no personal
+          data is stored in plain text or accessible without proper authorization.
         </ListItem>
         <ListItem>
-          <strong>Public Metadata:</strong> Metadata and information submitted by voting organizers may be publicly accessible on the InterPlanetary File System (IPFS). Organizers are responsible for compliance with applicable data protection laws.
+          <strong>Public Metadata:</strong> Metadata and information submitted by voting organizers may be publicly
+          accessible on the InterPlanetary File System (IPFS). Organizers are responsible for compliance with applicable
+          data protection laws.
         </ListItem>
         <ListItem>
-          <strong>Additional Data Collection:</strong> In certain cases, we collect additional data to fulfill legal obligations or provide specialized services. This data is collected with the user's explicit consent or as required by applicable laws.
+          <strong>Additional Data Collection:</strong> In certain cases, we collect additional data to fulfill legal
+          obligations or provide specialized services. This data is collected with the user's explicit consent or as
+          required by applicable laws.
         </ListItem>
       </UnorderedList>
 
-      <Heading as="h2" size="lg">3. Data Use and Compliance</Heading>
+      <Heading as='h2' size='lg'>
+        3. Data Use and Compliance
+      </Heading>
       <UnorderedList>
         <ListItem>
-          <strong>Purpose Limitation:</strong> Personal data is processed solely for providing and improving our services or fulfilling legal obligations. We do not process personal data for unrelated purposes.
+          <strong>Purpose Limitation:</strong> Personal data is processed solely for providing and improving our
+          services or fulfilling legal obligations. We do not process personal data for unrelated purposes.
         </ListItem>
         <ListItem>
-          <strong>User Consent:</strong> Where necessary, we seek explicit user consent for data processing activities. Consent may be withdrawn at any time by contacting us using the details provided below.
+          <strong>User Consent:</strong> Where necessary, we seek explicit user consent for data processing activities.
+          Consent may be withdrawn at any time by contacting us using the details provided below.
         </ListItem>
         <ListItem>
-          <strong>Marketing:</strong> We do not use collected data for marketing purposes without explicit user consent. Consent can be managed through user account settings.
+          <strong>Marketing:</strong> We do not use collected data for marketing purposes without explicit user consent.
+          Consent can be managed through user account settings.
         </ListItem>
       </UnorderedList>
 
-      <Heading as="h2" size="lg">4. Data Sharing</Heading>
+      <Heading as='h2' size='lg'>
+        4. Data Sharing
+      </Heading>
       <UnorderedList>
         <ListItem>
-          <strong>Third-Party Disclosure:</strong> Personal data is not shared with third parties except where required by law, for providing services (e.g., secure storage or customer support), or with the user's explicit consent.
+          <strong>Third-Party Disclosure:</strong> Personal data is not shared with third parties except where required
+          by law, for providing services (e.g., secure storage or customer support), or with the user's explicit
+          consent.
         </ListItem>
         <ListItem>
-          <strong>Data Processors:</strong> Third-party data processors are bound by data processing agreements that include confidentiality obligations consistent with GDPR requirements.
+          <strong>Data Processors:</strong> Third-party data processors are bound by data processing agreements that
+          include confidentiality obligations consistent with GDPR requirements.
         </ListItem>
         <ListItem>
-          <strong>Cross-Border Transfers:</strong> Any cross-border data transfers include adequate protection measures to ensure GDPR compliance.
+          <strong>Cross-Border Transfers:</strong> Any cross-border data transfers include adequate protection measures
+          to ensure GDPR compliance.
         </ListItem>
       </UnorderedList>
 
-      <Heading as="h2" size="lg">5. Security Measures</Heading>
-      <Text as="p">
-        The Vocdoni Association employs strong security measures to protect personal data. Only authorized personnel have access to user data, and they are obligated to handle it confidentially.
+      <Heading as='h2' size='lg'>
+        5. Security Measures
+      </Heading>
+      <Text>
+        The Vocdoni Association employs strong security measures to protect personal data. Only authorized personnel
+        have access to user data, and they are obligated to handle it confidentially.
       </Text>
-      <Text as="p">
-        In the event of a data breach that affects personal data, we will promptly notify affected individuals and relevant authorities as required by applicable laws.
+      <Text>
+        In the event of a data breach that affects personal data, we will promptly notify affected individuals and
+        relevant authorities as required by applicable laws.
       </Text>
 
-      <Heading as="h2" size="lg">6. User Rights</Heading>
+      <Heading as='h2' size='lg'>
+        6. User Rights
+      </Heading>
       <UnorderedList>
         <ListItem>
           <strong>Access:</strong> Obtain information on the processing of personal data and access the data we hold.
@@ -80,7 +111,8 @@ const Terms = () => {
           <strong>Restriction:</strong> Request that we restrict processing of personal data in specific situations.
         </ListItem>
         <ListItem>
-          <strong>Portability:</strong> Receive personal data in a structured format or request its transfer to a third party.
+          <strong>Portability:</strong> Receive personal data in a structured format or request its transfer to a third
+          party.
         </ListItem>
         <ListItem>
           <strong>Objection:</strong> Object to processing of personal data based on legitimate interests.
@@ -89,37 +121,48 @@ const Terms = () => {
           <strong>Complaint:</strong> Lodge a complaint with the relevant data protection authority.
         </ListItem>
       </UnorderedList>
-      <Text as="p">
-        To exercise these rights, contact us using the details provided below. We will respond promptly and fulfill your request as required by GDPR.
+      <Text>
+        To exercise these rights, contact us using the details provided below. We will respond promptly and fulfill your
+        request as required by GDPR.
       </Text>
 
-      <Heading as="h2" size="lg">7. Disclaimer</Heading>
-      <Text as="p">
-        The Vocdoni Association is not responsible for information submitted by end-users or organizers. Voting organizers must comply with applicable data protection laws.
+      <Heading as='h2' size='lg'>
+        7. Disclaimer
+      </Heading>
+      <Text>
+        The Vocdoni Association is not responsible for information submitted by end-users or organizers. Voting
+        organizers must comply with applicable data protection laws.
       </Text>
-      <Text as="p">
-        We do not track user data beyond what is essential for providing secure services.
-      </Text>
+      <Text>We do not track user data beyond what is essential for providing secure services.</Text>
 
-      <Heading as="h2" size="lg">8. Limitation of Liability</Heading>
+      <Heading as='h2' size='lg'>
+        8. Limitation of Liability
+      </Heading>
       <UnorderedList>
         <ListItem>
-          <strong>Indirect Damages:</strong> The Vocdoni Association shall not be liable for indirect, consequential, or incidental damages arising from the use of our services.
+          <strong>Indirect Damages:</strong> The Vocdoni Association shall not be liable for indirect, consequential, or
+          incidental damages arising from the use of our services.
         </ListItem>
         <ListItem>
-          <strong>Force Majeure:</strong> The Vocdoni Association is not responsible for delays or interruptions in service resulting from events beyond our reasonable control, including acts of God, natural disasters, or technical failures.
+          <strong>Force Majeure:</strong> The Vocdoni Association is not responsible for delays or interruptions in
+          service resulting from events beyond our reasonable control, including acts of God, natural disasters, or
+          technical failures.
         </ListItem>
       </UnorderedList>
 
-      <Heading as="h2" size="lg">9. Contact Information</Heading>
-      <Text as="p">
-        For any queries regarding these Terms and Conditions or to exercise your data rights, please contact us at info (at) vocdoni.org. We will respond promptly to address any concerns or complaints.
+      <Heading as='h2' size='lg'>
+        9. Contact Information
+      </Heading>
+      <Text>
+        For any queries regarding these Terms and Conditions or to exercise your data rights, please contact us at info
+        (at) vocdoni.org. We will respond promptly to address any concerns or complaints.
       </Text>
 
-      <Text as="p">
-        By continuing to use app.vocdoni.io or onvote.app, you confirm your acceptance of these terms and your agreement to abide by them.
+      <Text>
+        By continuing to use app.vocdoni.io or onvote.app, you confirm your acceptance of these terms and your agreement
+        to abide by them.
       </Text>
-    </Box>
+    </Flex>
   )
 }
 
