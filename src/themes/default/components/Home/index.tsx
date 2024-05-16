11ad43c60eefe9c5ffd68c614c8636d2cd3287b4
Gerard <g.roura.v@protonmail.com>
2024-05-16 09:54:28 +0200

    fixed issue bg home and navbar in local folder

--- src/themes/default/components/Home/index.tsx
+++ src/themes/default/components/Home/index.tsx
@@ -8,13 +8,11 @@ import Features from './Features'
 import Process from './Process'
 import Solutions from './Solutions'
 import Support from './Support'
+import bg from '/assets/bg-home.png'
 
 const Home = () => (
   <>
-    <Box
-      position='relative'
-      bgImage='url(https://cdn.discordapp.com/attachments/1077657962404925583/1232347225472897096/image.png?ex=662b1a8a&is=6629c90a&hm=1ecf6bf6ee44c37267943092f897e102c2d1e684723c0c4f3775c550939556d2&)'
-    >
+    <Box position='relative' bgImage={bg}>
       <Box position='absolute' top={0} left={0} right={0} bottom={0} backgroundColor='rgba(255, 255, 255, 0.7)'></Box>
       <Box position='relative' zIndex={10}>
         <CreateProcess />
