92a3af832f9f8bf6a1f4e9873f75f6e32c4c4314
selankon <selankon@selankon.xyz>
2024-05-23 09:20:42 +0200

    Delete comment

--- src/use-document-title.tsx
+++ src/use-document-title.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from 'react'
 
-// From https://github.com/uidotdev/usehooks/blob/main/index.js#L265-L269
-
 export function useDocumentTitle(title?: string) {
   const defaultTitleRef = useRef(document.title)
 
