5137665c993b9df261468f7ffcda983bca48ae72
selankon <selankon@selankon.xyz>
2024-06-17 11:56:56 +0200

    Fix go back to the original title

--- src/use-document-title.tsx
+++ src/use-document-title.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef } from 'react'
 
 export function useDocumentTitle(title?: string) {
-  const defaultTitleRef = useRef(document.title)
+  const defaultTitleRef = useRef(
+    import.meta.env.theme === 'onvote'
+      ? 'ONVOTE - Anonymous Gasless and Modular voting for Web3'
+      : 'Vocdoni - The voice of digital voting'
+  )
 
   useEffect(() => {
     if (title) {
       let prepend = 'Vocdoni'
       if (import.meta.env.theme === 'onvote') {
-        prepend = 'OnVote'
+        prepend = 'ONVOTE'
       }
       document.title = `${prepend} - ${title}`
     } else {
