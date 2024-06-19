0fdb48ce0f96607da4a8246f29f218577de0b823
selankon <selankon@selankon.xyz>
2024-06-19 12:23:11 +0200

    Delete scripts folder
    This folder is moved to demo projects https://github.com/vocdoni/projects/

--- scripts/src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const LOGO_DEST = '../public/default/assets/demo.png'
-export const DEMO_META_DEST = '../src/elements/landings/data.ts'
--- scripts/src/utils/spreadsheet/errors/ErrorCellData.ts
+++
@@ -1,9 +0,0 @@
-import ErrorRows from './ErrorRows'
-
-export default class ErrorCellData extends ErrorRows {
-  constructor(rows: number[]) {
-    super(rows)
-    this.message = 'error.invalid_cell_data: ' + this.rows;
-    this.name = 'ErrorCellData'
-  }
-}
