e0e73c134d709078dbe34f888549eda6391cd0b7
Gerard <g.roura.v@protonmail.com>
2024-06-25 15:09:43 +0200

    fixed issue translations

--- src/components/Organization/Dashboard/Create.tsx
+++ src/components/Organization/Dashboard/Create.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@vocdoni/chakra-components'
 import { useClient } from '@vocdoni/react-providers'
+import { useTranslation } from 'react-i18next'
 import { AccountCreate } from '~components/Account/Create'
 import { ContentsBox } from './Box'
 
 const CreateOrganization = () => {
+  const { t } = useTranslation()
+
   const {
     loading: { create },
   } = useClient()
@@ -12,7 +15,7 @@ const CreateOrganization = () => {
     <ContentsBox>
       <AccountCreate />
       <Button form='process-create-form' type='submit' isLoading={create} mx='auto'>
-        Create org
+        {t('organization.create_org')}
       </Button>
     </ContentsBox>
   )
