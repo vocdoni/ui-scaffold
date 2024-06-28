d51434e06f123c4d1a6a869a13aa10ef544b2b5a
emmdim <emmdim@users.noreply.github.com>
2024-06-28 15:03:15 +0200

    Stripe: Repeats faucet getSession call until a faucetPackage is provided

--- src/elements/Stripe.tsx
+++ src/elements/Stripe.tsx
@@ -1,12 +1,21 @@
 import { Box } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
-import { CheckoutForm } from '~components/Faucet/Stripe'
+import { CheckoutForm, CheckoutReturn } from '~components/Faucet/Stripe'
 
-export const Checkout = () => {
+export const StripeCheckout = () => {
   const { amount }: { amount?: string } = useParams()
   return (
     <Box>
       <CheckoutForm amount={amount} />
+    </Box>
+  )
+}
+
+export const StripeReturn = () => {
+  const { sessionId }: { sessionId?: string } = useParams()
+  return (
+    <Box>
+      <CheckoutReturn sessionId={sessionId || ''} />
     </Box>
   )
 }
