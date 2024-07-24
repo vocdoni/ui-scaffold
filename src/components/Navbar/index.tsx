6d300273f793962bb89fb99bd8f2342f56a617e9
Gerard <g.roura.v@protonmail.com>
2024-07-24 12:24:40 +0200

    Created empty saas theme

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,6 @@ import { Link as ReactRouterLink } from 'react-router-dom'
 import { useAccount } from 'wagmi'
 import { useOrganizationHealthTools } from '~components/Account/use-account-health-tools'
 import Logo from '~components/Layout/Logo'
-import { NavBg } from '~theme/bg'
 import { LanguagesMenu } from './LanguagesList'
 import MenuDropdown from './Menu'
 
@@ -20,87 +19,85 @@ const Navbar = () => {
   const { exists } = useOrganizationHealthTools()
 
   return (
-    <NavBg>
-      <Flex className='site-wrapper' w='full' mx='auto' py={{ base: '12px', md: '24px' }} position='relative'>
-        <Flex justifyContent='space-between' alignItems='center' zIndex={1} w='100%'>
-          <Logo />
+    <Flex className='site-wrapper' w='full' mx='auto' py={{ base: '12px', md: '24px' }} position='relative'>
+      <Flex justifyContent='space-between' alignItems='center' zIndex={1} w='100%'>
+        <Logo />
 
-          <List as='nav' display='flex' alignItems='center' gap={4}>
-            {isConnected && (
+        <List as='nav' display='flex' alignItems='center' gap={4}>
+          {isConnected && (
+            <ListItem>
+              <Button as={ReactRouterLink} to='/processes/create'>
+                <AddIcon boxSize={{ base: 4, sm2: 3 }} />
+                <Text as='span' display={{ base: 'none', sm2: 'inline-block' }}>
+                  {t('menu.new_process')}
+                </Text>
+              </Button>
+            </ListItem>
+          )}
+
+          {isConnected && (
+            <ListItem>
+              <Button as={ReactRouterLink} to={`/organization`} variant='secondary'>
+                <Icon as={MdHowToVote} boxSize={{ base: 4, sm2: 3 }} />
+                <Text as='span' display={{ base: 'none', sm2: 'inline-block' }}>
+                  {t('menu.my_org')}
+                </Text>
+              </Button>
+            </ListItem>
+          )}
+
+          {!isConnected && (
+            <>
               <ListItem>
-                <Button as={ReactRouterLink} to='/processes/create'>
-                  <AddIcon boxSize={{ base: 4, sm2: 3 }} />
-                  <Text as='span' display={{ base: 'none', sm2: 'inline-block' }}>
-                    {t('menu.new_process')}
-                  </Text>
+                <Button
+                  onClick={() => {
+                    if (openConnectModal) openConnectModal()
+                  }}
+                  width='175px'
+                  height='50px'
+                >
+                  {t('menu.login').toString()}
                 </Button>
               </ListItem>
-            )}
-
-            {isConnected && (
               <ListItem>
-                <Button as={ReactRouterLink} to={`/organization`} variant='secondary'>
-                  <Icon as={MdHowToVote} boxSize={{ base: 4, sm2: 3 }} />
-                  <Text as='span' display={{ base: 'none', sm2: 'inline-block' }}>
-                    {t('menu.my_org')}
-                  </Text>
-                </Button>
+                <LanguagesMenu />
               </ListItem>
-            )}
-
-            {!isConnected && (
-              <>
-                <ListItem>
-                  <Button
-                    onClick={() => {
-                      if (openConnectModal) openConnectModal()
-                    }}
-                    width='175px'
-                    height='50px'
-                  >
-                    {t('menu.login').toString()}
-                  </Button>
-                </ListItem>
-                <ListItem>
-                  <LanguagesMenu />
-                </ListItem>
-              </>
-            )}
-            {isConnected && (
-              <ListItem>
-                <Menu>
-                  {({ isOpen }) => (
-                    <>
-                      <MenuButton
-                        as={Button}
-                        variant='secondary'
-                        boxShadow='var(--box-shadow-btn)'
-                        aria-label={t('menu.languages_list')}
-                        p={2}
-                      >
-                        <Box as='span' display='flex' alignItems='center'>
-                          <Avatar
-                            src={account?.account.avatar}
-                            name={account?.account.name.default || account?.address}
-                            size='xs'
-                          />
-                          {isOpen ? (
-                            <ChevronUpIcon boxSize={8} color='navbar_chevron' />
-                          ) : (
-                            <ChevronDownIcon boxSize={8} color='navbar_chevron' />
-                          )}
-                        </Box>
-                      </MenuButton>
-                      <MenuDropdown />
-                    </>
-                  )}
-                </Menu>
-              </ListItem>
-            )}
-          </List>
-        </Flex>
+            </>
+          )}
+          {isConnected && (
+            <ListItem>
+              <Menu>
+                {({ isOpen }) => (
+                  <>
+                    <MenuButton
+                      as={Button}
+                      variant='secondary'
+                      boxShadow='var(--box-shadow-btn)'
+                      aria-label={t('menu.languages_list')}
+                      p={2}
+                    >
+                      <Box as='span' display='flex' alignItems='center'>
+                        <Avatar
+                          src={account?.account.avatar}
+                          name={account?.account.name.default || account?.address}
+                          size='xs'
+                        />
+                        {isOpen ? (
+                          <ChevronUpIcon boxSize={8} color='navbar_chevron' />
+                        ) : (
+                          <ChevronDownIcon boxSize={8} color='navbar_chevron' />
+                        )}
+                      </Box>
+                    </MenuButton>
+                    <MenuDropdown />
+                  </>
+                )}
+              </Menu>
+            </ListItem>
+          )}
+        </List>
       </Flex>
-    </NavBg>
+    </Flex>
   )
 }
 
---
+++ src/themes/saas/index.ts
@@ -0,0 +1,28 @@
+import { ColorMode, extendTheme } from '@chakra-ui/react'
+import { darkTheme, lightTheme } from '@rainbow-me/rainbowkit'
+import { theme as vtheme } from '@vocdoni/chakra-components'
+import { shared } from '../shared'
+
+export const theme = extendTheme(vtheme, {
+  styles: {
+    global: {
+      ...shared,
+    },
+  },
+})
+
+export const rainbowStyles = (colormode: ColorMode) => {
+  if (colormode === 'light') {
+    return lightTheme({
+      accentColor: 'white',
+      accentColorForeground: 'green',
+      borderRadius: 'medium',
+    })
+  }
+
+  return darkTheme({
+    accentColor: 'white',
+    accentColorForeground: 'green',
+    borderRadius: 'medium',
+  })
+}
