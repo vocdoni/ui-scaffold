71caaebe09bbf1d07288a8c9bd3f8026fd71cf39
Gerard <g.roura.v@protonmail.com>
2024-07-24 12:34:46 +0200

    Added core styles

--- src/themes/saas/index.ts
+++ src/themes/saas/index.ts
@@ -2,6 +2,17 @@ import { ColorMode, extendTheme } from '@chakra-ui/react'
 import { darkTheme, lightTheme } from '@rainbow-me/rainbowkit'
 import { theme as vtheme } from '@vocdoni/chakra-components'
 import { shared } from '../shared'
+import { CardComponent } from './additions/card/card'
+import { badgeStyles } from './components/badge'
+import { buttonStyles } from './components/button'
+import { inputStyles } from './components/input'
+import { linkStyles } from './components/link'
+import { progressStyles } from './components/progress'
+import { sliderStyles } from './components/slider'
+import { switchStyles } from './components/switch'
+import { textareaStyles } from './components/textarea'
+import { breakpoints } from './foundations/breakpoints'
+import { globalStyles } from './styles'
 
 export const theme = extendTheme(vtheme, {
   styles: {
@@ -9,6 +20,17 @@ export const theme = extendTheme(vtheme, {
       ...shared,
     },
   },
+  breakpoints, // Breakpoints
+  globalStyles,
+  badgeStyles, // badge styles
+  buttonStyles, // button styles
+  linkStyles, // link styles
+  progressStyles, // progress styles
+  sliderStyles, // slider styles
+  inputStyles, // input styles
+  textareaStyles, // textarea styles
+  switchStyles, // switch styles
+  CardComponent, // card component
 })
 
 export const rainbowStyles = (colormode: ColorMode) => {
