5b4f52b26459e90ec043990687a3de44b926d5b1
selankon <selankon@selankon.xyz>
2024-09-16 11:56:41 +0200

    Fix sign in redirection

--- src/router/SignInScreen.tsx
+++ src/router/SignInScreen.tsx
@@ -1,9 +1,15 @@
 import { Button, Flex, Text } from '@chakra-ui/react'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
 import { Trans } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 
 const SignInScreen = () => {
   const { openConnectModal } = useConnectModal()
+  const navigate = useNavigate()
+
+  if (!!import.meta.env.SAAS_URL) {
+    return navigate('/signin') // Redirect to the SAAS sign-in page
+  }
 
   return (
     <Flex gap={4} justifyContent='center' alignItems='center' flexDirection='column' h='100%'>
