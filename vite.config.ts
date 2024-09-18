bacb7a32f385bb872514c43df41f560263fb737b
selankon <selankon@selankon.xyz>
2024-09-18 08:22:40 +0200

    Revert "Install node polyfills"
    This reverts commit abae66494bd91f8b9c50169164dd69b9b667a128.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react'
 import { execSync } from 'node:child_process'
 import { defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import features from './vite/features'
@@ -72,7 +71,6 @@ const viteconfig = ({ mode }) => {
           },
         },
       }),
-      nodePolyfills(),
     ],
   })
 }
