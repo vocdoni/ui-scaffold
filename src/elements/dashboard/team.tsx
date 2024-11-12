cdbe2e04e81ba562c6d596937dea1495c2d9dab4
gerouvi <78085338+gerouvi@users.noreply.github.com>
2024-11-12 10:19:09 +0100

    dashboard style issues and unused features (#832)
    * fixed issues related #825
    
    * fixed issues
    
    * fixed issue, process card pl
    
    * change process card link pl to px

--- src/elements/dashboard/team.tsx
+++ src/elements/dashboard/team.tsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useOutletContext } from 'react-router-dom'
 import { DashboardContents } from '~components/Layout/Dashboard'
 import Team from '~components/Organization/Dashboard/Team'
+import { DashboardLayoutContext } from '~elements/LayoutDashboard'
 
-const OrganizationTeam = () => (
-  <DashboardContents>
-    <Team />
-  </DashboardContents>
-)
+const OrganizationTeam = () => {
+  const { t } = useTranslation()
+  const { setTitle } = useOutletContext<DashboardLayoutContext>()
+
+  // Set layout variables
+  useEffect(() => {
+    setTitle(t('team', { defaultValue: 'Team' }))
+  }, [setTitle])
+
+  return (
+    <DashboardContents>
+      <Team />
+    </DashboardContents>
+  )
+}
 
 export default OrganizationTeam
