8b50bb29022f91d390160422718744a879a22bfb
Òscar Casajuana <elboletaire@underave.net>
2024-11-14 11:44:30 +0100

    Processes filtering logic (#835)
    * Add processes pagination filter by status
    
    And updated the sidebar links to point to these new filters
    
    * Remove unused method
    
    * Minor varname change
    
    * Review fixes

--- src/queries/organization.ts
+++ src/queries/organization.ts
@@ -1,24 +1,21 @@
-import { useQuery } from '@tanstack/react-query'
-import { useClient } from '@vocdoni/react-providers'
+import { AccountData, FetchElectionsParameters, VocdoniSDKClient } from '@vocdoni/sdk'
 
-export const useLatestElections = (limit = 5) => {
-  const { client, account } = useClient()
-
-  return useQuery({
-    enabled: !!account?.address,
-    queryKey: ['organization', 'elections', account?.address, 0],
-    queryFn: async () => client.fetchElections({ organizationId: account?.address, page: 0, limit }),
-    select: (data) => data.elections,
-    retry: false,
-  })
+type PaginatedElectionsParams = {
+  page?: number
+  status?: FetchElectionsParameters['status']
 }
 
-export const usePaginatedElections = (page: number) => {
-  const { client, account } = useClient()
-
-  return useQuery({
-    enabled: !!account?.address,
-    queryKey: ['organization', 'elections', account?.address, page],
-    queryFn: async () => client.fetchElections({ organizationId: account?.address, page }),
-  })
-}
+export const paginatedElectionsQuery = (
+  account: AccountData,
+  client: VocdoniSDKClient,
+  params: PaginatedElectionsParams
+) => ({
+  enabled: !!account?.address,
+  queryKey: ['organization', 'elections', account?.address, params],
+  queryFn: async () =>
+    client.fetchElections({
+      organizationId: account?.address,
+      page: params.page ? Number(params.page) - 1 : 0,
+      status: params.status?.toUpperCase() as FetchElectionsParameters['status'],
+    }),
+})
