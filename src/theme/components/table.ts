5f8f9153f2a3dd924f47959aa4a569b03eecd7e3
Gerard <g.roura.v@protonmail.com>
2024-11-21 13:44:15 +0100

    WIP

--- src/theme/components/table.ts
+++ src/theme/components/table.ts
@@ -4,26 +4,29 @@ import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
 const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(tableAnatomy.keys)
 
 const baseStyle = definePartsStyle((props) => ({
+  container: {
+    border: '3px solid red',
+  },
   table: {
     borderRadius: 'lg',
-    bgColor: 'white',
-    overflow: 'hidden',
   },
-  thead: {
-    bg: 'gray.100',
-    bgColor: 'lightBlue',
-  },
-  th: {
-    textTransform: 'normal',
-    fontWeight: 600,
-    textAlign: 'left',
-  },
+  thead: {},
+  th: {},
   td: {},
 }))
 
 const md = definePartsStyle({
+  table: {
+    maxW: '300px !important',
+    overflowX: 'scroll',
+    // border: '2px solid red',
+  },
   th: {
-    paddingTop: '160px',
+    paddingY: '24px',
+  },
+  td: {
+    color: 'red',
+    whiteSpace: 'nowrap',
   },
 })
 
