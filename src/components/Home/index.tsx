c882fc16caf10e57ecd5a79503931282154d882b
Gerard <g.roura.v@protonmail.com>
2024-12-04 20:25:14 +0100

    remove home alert

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Benefits from './Benefits'
 import Clients from './Clients'
 import ContactUs from './ContactUs'
@@ -13,11 +13,6 @@ const Home = () => (
   <>
     <Box position='relative'>
       <Box position='relative' zIndex={10}>
-        <Alert status='error'>
-          <AlertIcon />
-          <AlertTitle>TITLE</AlertTitle>
-          <AlertDescription>DESCRIPTION</AlertDescription>
-        </Alert>
         <CreateProcess />
         <Clients />
         <Benefits />
