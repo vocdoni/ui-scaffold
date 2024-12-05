617867eb590e82f4d2f29d095ed295e4a687a701
Òscar Casajuana <elboletaire@gmail.com>
2024-12-05 13:14:07 +0100

    Fix how i18next was being used in terms and conditions

--- src/components/TermsAndPrivacy/Privacy.tsx
+++ src/components/TermsAndPrivacy/Privacy.tsx
@@ -1,86 +1,89 @@
 import { Box, Heading, Link, List, ListItem, Text } from '@chakra-ui/react'
-import { t } from 'i18next'
+import { useTranslation } from 'react-i18next'
 
-const Privacy = () => (
-  <>
-    <Box mb={5}>
-      <Heading as='h1' size='xl' mb={3}>
-        {t('privacyPolicy.title')}
-      </Heading>
-      <Text>{t('privacyPolicy.description')}</Text>
-    </Box>
+const Privacy = () => {
+  const { t } = useTranslation()
+  return (
+    <>
+      <Box mb={5}>
+        <Heading as='h1' size='xl' mb={3}>
+          {t('privacyPolicy.title')}
+        </Heading>
+        <Text>{t('privacyPolicy.description')}</Text>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section1Title')}
-      </Heading>
-      <List spacing={2}>
-        <ListItem>
-          <Text>
-            <strong>1.1</strong> {t('privacyPolicy.section1.item1')}
-          </Text>
-        </ListItem>
-        <ListItem>
-          <Text>
-            <strong>1.2</strong> {t('privacyPolicy.section1.item2')}
-          </Text>
-        </ListItem>
-        <ListItem>
-          <Text>
-            <strong>1.3</strong> {t('privacyPolicy.section1.item3')}
-          </Text>
-        </ListItem>
-      </List>
-    </Box>
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section1Title')}
+        </Heading>
+        <List spacing={2}>
+          <ListItem>
+            <Text>
+              <strong>1.1</strong> {t('privacyPolicy.section1.item1')}
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text>
+              <strong>1.2</strong> {t('privacyPolicy.section1.item2')}
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text>
+              <strong>1.3</strong> {t('privacyPolicy.section1.item3')}
+            </Text>
+          </ListItem>
+        </List>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section2Title')}
-      </Heading>
-      <Text>{t('privacyPolicy.section2.description')}</Text>
-    </Box>
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section2Title')}
+        </Heading>
+        <Text>{t('privacyPolicy.section2.description')}</Text>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section3Title')}
-      </Heading>
-      <Text>{t('privacyPolicy.section3.description')}</Text>
-    </Box>
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section3Title')}
+        </Heading>
+        <Text>{t('privacyPolicy.section3.description')}</Text>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section4Title')}
-      </Heading>
-      <Text>{t('privacyPolicy.section4.description')}</Text>
-    </Box>
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section4Title')}
+        </Heading>
+        <Text>{t('privacyPolicy.section4.description')}</Text>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section5Title')}
-      </Heading>
-      <Text>{t('privacyPolicy.section5.description')}</Text>
-    </Box>
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section5Title')}
+        </Heading>
+        <Text>{t('privacyPolicy.section5.description')}</Text>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section6Title')}
-      </Heading>
-      <Text>{t('privacyPolicy.section6.description')}</Text>
-    </Box>
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section6Title')}
+        </Heading>
+        <Text>{t('privacyPolicy.section6.description')}</Text>
+      </Box>
 
-    <Box mb={5}>
-      <Heading as='h2' size='lg' mb={2}>
-        {t('privacyPolicy.section7Title')}
-      </Heading>
-      <Text>
-        {t('privacyPolicy.section7.description')}{' '}
-        <Link href='mailto:privacy@vocdoni.org' color='teal.500'>
-          privacy@vocdoni.org
-        </Link>
-        .
-      </Text>
-    </Box>
-  </>
-)
+      <Box mb={5}>
+        <Heading as='h2' size='lg' mb={2}>
+          {t('privacyPolicy.section7Title')}
+        </Heading>
+        <Text>
+          {t('privacyPolicy.section7.description')}{' '}
+          <Link href='mailto:privacy@vocdoni.org' color='teal.500'>
+            privacy@vocdoni.org
+          </Link>
+          .
+        </Text>
+      </Box>
+    </>
+  )
+}
 
 export default Privacy
--- src/components/TermsAndPrivacy/Terms.tsx
+++ src/components/TermsAndPrivacy/Terms.tsx
@@ -1,87 +1,91 @@
 import { Heading, ListItem, Text, UnorderedList } from '@chakra-ui/react'
-import { t } from 'i18next'
+import { useTranslation } from 'react-i18next'
 
-const Terms = () => (
-  <>
-    <Heading as='h1' size='xl'>
-      {t('terms.title')}
-    </Heading>
+const Terms = () => {
+  const { t } = useTranslation()
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section1Title')}
-    </Heading>
-    <Text>{t('terms.section1.description1')}</Text>
-    <Text>{t('terms.section1.description2')}</Text>
+  return (
+    <>
+      <Heading as='h1' size='xl'>
+        {t('terms.title')}
+      </Heading>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section2Title')}
-    </Heading>
-    <Text>{t('terms.section2.description')}</Text>
-    <UnorderedList>
-      <ListItem>{t('terms.section2.item1')}</ListItem>
-      <ListItem>{t('terms.section2.item2')}</ListItem>
-      <ListItem>{t('terms.section2.item3')}</ListItem>
-      <ListItem>{t('terms.section2.item4')}</ListItem>
-    </UnorderedList>
+      <Heading as='h2' size='lg'>
+        {t('terms.section1Title')}
+      </Heading>
+      <Text>{t('terms.section1.description1')}</Text>
+      <Text>{t('terms.section1.description2')}</Text>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section3Title')}
-    </Heading>
-    <UnorderedList>
-      <ListItem>{t('terms.section3.item1')}</ListItem>
-      <ListItem>{t('terms.section3.item2')}</ListItem>
-      <ListItem>{t('terms.section3.item3')}</ListItem>
-    </UnorderedList>
+      <Heading as='h2' size='lg'>
+        {t('terms.section2Title')}
+      </Heading>
+      <Text>{t('terms.section2.description')}</Text>
+      <UnorderedList>
+        <ListItem>{t('terms.section2.item1')}</ListItem>
+        <ListItem>{t('terms.section2.item2')}</ListItem>
+        <ListItem>{t('terms.section2.item3')}</ListItem>
+        <ListItem>{t('terms.section2.item4')}</ListItem>
+      </UnorderedList>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section4Title')}
-    </Heading>
-    <UnorderedList>
-      <ListItem>{t('terms.section4.item1')}</ListItem>
-      <ListItem>{t('terms.section4.item2')}</ListItem>
-      <ListItem>{t('terms.section4.item3')}</ListItem>
-    </UnorderedList>
+      <Heading as='h2' size='lg'>
+        {t('terms.section3Title')}
+      </Heading>
+      <UnorderedList>
+        <ListItem>{t('terms.section3.item1')}</ListItem>
+        <ListItem>{t('terms.section3.item2')}</ListItem>
+        <ListItem>{t('terms.section3.item3')}</ListItem>
+      </UnorderedList>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section5Title')}
-    </Heading>
-    <Text>{t('terms.section5.description1')}</Text>
-    <Text>{t('terms.section5.description2')}</Text>
+      <Heading as='h2' size='lg'>
+        {t('terms.section4Title')}
+      </Heading>
+      <UnorderedList>
+        <ListItem>{t('terms.section4.item1')}</ListItem>
+        <ListItem>{t('terms.section4.item2')}</ListItem>
+        <ListItem>{t('terms.section4.item3')}</ListItem>
+      </UnorderedList>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section6Title')}
-    </Heading>
-    <UnorderedList>
-      <ListItem>{t('terms.section6.item1')}</ListItem>
-      <ListItem>{t('terms.section6.item2')}</ListItem>
-      <ListItem>{t('terms.section6.item3')}</ListItem>
-      <ListItem>{t('terms.section6.item4')}</ListItem>
-      <ListItem>{t('terms.section6.item5')}</ListItem>
-      <ListItem>{t('terms.section6.item6')}</ListItem>
-      <ListItem>{t('terms.section6.item7')}</ListItem>
-    </UnorderedList>
-    <Text>{t('terms.section6.description')}</Text>
+      <Heading as='h2' size='lg'>
+        {t('terms.section5Title')}
+      </Heading>
+      <Text>{t('terms.section5.description1')}</Text>
+      <Text>{t('terms.section5.description2')}</Text>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section7Title')}
-    </Heading>
-    <Text>{t('terms.section7.description1')}</Text>
-    <Text>{t('terms.section7.description2')}</Text>
+      <Heading as='h2' size='lg'>
+        {t('terms.section6Title')}
+      </Heading>
+      <UnorderedList>
+        <ListItem>{t('terms.section6.item1')}</ListItem>
+        <ListItem>{t('terms.section6.item2')}</ListItem>
+        <ListItem>{t('terms.section6.item3')}</ListItem>
+        <ListItem>{t('terms.section6.item4')}</ListItem>
+        <ListItem>{t('terms.section6.item5')}</ListItem>
+        <ListItem>{t('terms.section6.item6')}</ListItem>
+        <ListItem>{t('terms.section6.item7')}</ListItem>
+      </UnorderedList>
+      <Text>{t('terms.section6.description')}</Text>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section8Title')}
-    </Heading>
-    <UnorderedList>
-      <ListItem>{t('terms.section8.item1')}</ListItem>
-      <ListItem>{t('terms.section8.item2')}</ListItem>
-    </UnorderedList>
+      <Heading as='h2' size='lg'>
+        {t('terms.section7Title')}
+      </Heading>
+      <Text>{t('terms.section7.description1')}</Text>
+      <Text>{t('terms.section7.description2')}</Text>
 
-    <Heading as='h2' size='lg'>
-      {t('terms.section9Title')}
-    </Heading>
-    <Text>{t('terms.section9.description1')}</Text>
-    <Text>{t('terms.section9.description2')}</Text>
-  </>
-)
+      <Heading as='h2' size='lg'>
+        {t('terms.section8Title')}
+      </Heading>
+      <UnorderedList>
+        <ListItem>{t('terms.section8.item1')}</ListItem>
+        <ListItem>{t('terms.section8.item2')}</ListItem>
+      </UnorderedList>
+
+      <Heading as='h2' size='lg'>
+        {t('terms.section9Title')}
+      </Heading>
+      <Text>{t('terms.section9.description1')}</Text>
+      <Text>{t('terms.section9.description2')}</Text>
+    </>
+  )
+}
 
 export default Terms
