9cb292d0682a83d72b085500271b9e2cd1d52e03
Gerard <g.roura.v@protonmail.com>
2024-12-18 13:36:38 +0100

    removed tabs example data

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -9,15 +9,6 @@ import Process from './Process'
 import Solutions from './Solutions'
 import Support from './Support'
 
-const exampleTabs = {
-  tabList: ['Tab1', 'Tab2', 'Tab3'],
-  tabPanels: [
-    'Tab1 Panel',
-    'Tab2 Panel',
-    'Tab3 Panel Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eufugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseruntmollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velitesse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpaqui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatcupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  ],
-}
-
 const Home = () => (
   <>
     <Box position='relative'>
