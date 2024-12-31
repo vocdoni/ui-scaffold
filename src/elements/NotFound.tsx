be09a6bf5e209470d9854c0be6a96e6473e35af0
gerouvi <78085338+gerouvi@users.noreply.github.com>
2024-12-31 10:46:50 +0100

    404 page (#906)

--- src/elements/NotFound.tsx
+++ src/elements/NotFound.tsx
@@ -1,4 +1,4 @@
-import { Flex, Link, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import { Routes } from '~src/router/routes'
@@ -10,39 +10,40 @@ const NotFound = () => {
   const { idx } = window.history.state ?? false
 
   return (
-    <Flex
-      flexDirection='column'
-      justifyContent='center'
-      alignItems='center'
-      pt={20}
-      mb={44}
-      px={{
-        base: 10,
-        sm: 14,
-      }}
-    >
+    <Flex flexGrow={1} position={'relative'} minH={'70vh'}>
       <Text
-        fontSize={{ base: 'xl11', sm: 'xl12' }}
-        lineHeight={0.9}
-        textAlign='center'
-        p={0}
-        bgGradient='var(--vcd-gradient-primary)'
-        bgClip='text'
-        fontWeight='bold'
+        position={'absolute'}
+        left={'50%'}
+        top={'50%'}
+        transform='translate(-50%, -50%)'
+        fontSize={{ base: '200px', md: '350px', lg: '450px' }}
+        color={'#E1E8F0'}
+        _dark={{
+          color: '#22262f',
+        }}
       >
         404
       </Text>
-      <Text
-        textAlign='center'
-        maxW={{ base: 84, sm: 112, lg: 160 }}
-        fontWeight='bold'
-        fontSize={{ base: 'xl4', sm: 'xl5', lg: 'xl7' }}
-      >
-        {t('error.not_found')}
-      </Text>
-      <Link onClick={() => (idx ? navigate(-1) : navigate(Routes.root))}>
-        {idx ? t('error.go_back') : t('error.return_to_home')}
-      </Link>
+      <Box position={'absolute'} left={'50%'} top={'50%'} transform='translate(-50%, -50%)'>
+        <Text
+          textAlign='center'
+          fontWeight='bold'
+          fontSize={{ base: '24px', md: '46px', lg: '60px' }}
+          mb={{ base: 4, lg: 14 }}
+          whiteSpace={'nowrap'}
+        >
+          {t('error.not_found')}
+        </Text>
+
+        <Flex gap={2} justifyContent={'center'}>
+          {!idx && (
+            <Button variant='outline' onClick={() => navigate(-1)}>
+              {t('error.go_back')}
+            </Button>
+          )}
+          <Button onClick={() => navigate(Routes.root)}>{t('error.return_to_home')}</Button>
+        </Flex>
+      </Box>
     </Flex>
   )
 }
