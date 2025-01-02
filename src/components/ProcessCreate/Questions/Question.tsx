8cd4934a5f0399035fe61427d4031510668b8029
Gerard <g.roura.v@protonmail.com>
2025-01-02 21:45:44 +0100

    removed duplicated btn

--- src/components/ProcessCreate/Questions/Question.tsx
+++ src/components/ProcessCreate/Questions/Question.tsx
@@ -60,16 +60,6 @@ export const Question = ({ index, isMultiQuestion, remove }: Props) => {
           />
           <FormErrorMessage>{fieldMapErrorMessage(errors, `questions.${index}.title`)}</FormErrorMessage>
         </FormControl>
-
-        {questions?.length > 1 && (
-          <IconButton
-            type='button'
-            icon={<DeleteIcon />}
-            aria-label={t('form.process_create.question.question_delete', { values: { num: index } })}
-            onClick={() => remove(index)}
-            colorScheme='red'
-          />
-        )}
       </Flex>
       <Editor
         onChange={(text: string) => setValue(`questions.${index}.description`, text)}
