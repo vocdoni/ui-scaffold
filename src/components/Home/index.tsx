1f304ce60f96e4d86757386094f668105c535768
Gerard <g.roura.v@protonmail.com>
2025-01-03 11:06:42 +0100

    removed badge display flex

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Badge, Box } from '@chakra-ui/react'
 import Benefits from './Benefits'
 import Clients from './Clients'
 import ContactUs from './ContactUs'
@@ -13,6 +13,7 @@ const Home = () => (
   <>
     <Box position='relative'>
       <Box position='relative' zIndex={10}>
+        <Badge>HELLO</Badge>
         <CreateProcess />
         <Clients />
         <Benefits />
