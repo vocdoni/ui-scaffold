3a8e718038abfd41f0026d2b89ce882a92b0eed4
gerouvi <78085338+gerouvi@users.noreply.github.com>
2025-01-15 11:05:07 +0100

    Dashboard styles fixes (#927)
    * styles dashboard menu, account menu and box mb
    
    * remove comments
    
    * refactor
    
    * moved LanguagesListAccordion to LanguageList file
    
    * DropdownColorModeSwitcher component
    
    * fixed issues linter

--- src/components/Account/Menu.tsx
+++ src/components/Account/Menu.tsx
@@ -1,9 +1,10 @@
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 import {
   Avatar,
   Box,
   BoxProps,
+  Button,
   Icon,
-  IconButton,
   Menu,
   MenuButton,
   MenuDivider,
@@ -15,6 +16,8 @@ import { LogOut01, Paperclip, UserSquare } from '@untitled-ui/icons-react'
 import { Trans } from 'react-i18next'
 import { Link as RouterLink } from 'react-router-dom'
 import { useAuth } from '~components/Auth/useAuth'
+import { DropdownColorModeSwitcher } from '~components/Layout/ColorModeSwitcher'
+import LanguagesListAccordion from '~components/Navbar/LanguagesList'
 import { useProfile } from '~src/queries/account'
 import { Routes } from '~src/router/routes'
 
@@ -33,40 +36,53 @@ const AccountMenu: React.FC<BoxProps> = (props) => {
   return (
     <Box {...props}>
       <Menu>
-        <MenuButton
-          as={IconButton}
-          icon={
-            <Avatar
-              name={`${profile.firstName} ${profile.lastName}`}
-              src={profile.organizations[0]?.organization.logo || ''}
-              size='sm'
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              as={Button}
+              leftIcon={
+                <Avatar
+                  name={`${profile.firstName} ${profile.lastName}`}
+                  src={profile.organizations[0]?.organization.logo || ''}
+                  size='sm'
+                />
+              }
+              rightIcon={isOpen ? <ChevronUpIcon boxSize={7} /> : <ChevronDownIcon boxSize={7} />}
+              aria-label='User menu'
+              sx={{ '& > span': { m: 0 } }}
+              display={'flex'}
+              alignItems={'center'}
+              variant={'unstyled'}
             />
-          }
-          variant='link'
-          aria-label='User menu'
-        />
-        <MenuList>
-          <MenuItem as={RouterLink} to={Routes.dashboard.profile}>
-            <Icon as={UserSquare} />
-            <Trans i18nKey='profile.title'>Profile</Trans>
-          </MenuItem>
-          <MenuItem as={RouterLink} to='https://developer.vocdoni.io/' target='_blank'>
-            <Icon as={Paperclip} />
-            <Trans i18nKey='menu.documentation'>Documentation</Trans>
-          </MenuItem>
-          <MenuDivider />
-          <MenuItem onClick={logout}>
-            <Icon as={LogOut01} />
-            <Trans i18nKey='logout'>Logout</Trans>
-          </MenuItem>
-          <MenuDivider />
-          <MenuItem fontSize={'xs'} as={RouterLink} to={Routes.terms}>
-            <Trans i18nKey='menu.terms'>Terms</Trans>
-          </MenuItem>
-          <MenuItem fontSize={'xs'} as={RouterLink} to={Routes.privacy}>
-            <Trans i18nKey='menu.privacy'>Privacy</Trans>
-          </MenuItem>
-        </MenuList>
+            <MenuList>
+              <MenuItem as={RouterLink} to={Routes.dashboard.profile} closeOnSelect={true}>
+                <Icon as={UserSquare} />
+                <Trans i18nKey='profile.title'>Profile</Trans>
+              </MenuItem>
+              <MenuItem as={RouterLink} to='https://developer.vocdoni.io/' target='_blank' closeOnSelect={true}>
+                <Icon as={Paperclip} />
+                <Trans i18nKey='menu.documentation'>Documentation</Trans>
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem closeOnSelect={false}>
+                <LanguagesListAccordion />
+              </MenuItem>
+              <DropdownColorModeSwitcher />
+              <MenuDivider />
+              <MenuItem onClick={logout} closeOnSelect={true}>
+                <Icon as={LogOut01} />
+                <Trans i18nKey='logout'>Logout</Trans>
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem fontSize={'xs'} as={RouterLink} to={Routes.terms} closeOnSelect={true}>
+                <Trans i18nKey='menu.terms'>Terms</Trans>
+              </MenuItem>
+              <MenuItem fontSize={'xs'} as={RouterLink} to={Routes.privacy} closeOnSelect={true}>
+                <Trans i18nKey='menu.privacy'>Privacy</Trans>
+              </MenuItem>
+            </MenuList>
+          </>
+        )}
       </Menu>
     </Box>
   )
