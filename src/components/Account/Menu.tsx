5e2daa1d998869b8ffb98b763c9b95ecd184fc8a
gerouvi <78085338+gerouvi@users.noreply.github.com>
2025-01-31 16:31:40 +0100

    fix: dashboard aside button gap (#971)
    * fixed issue gap dashboard aside
    
    * min w aside, and spinner
    
    * minor fixes
    
    ---------
    
    Co-authored-by: Òscar Casajuana <elboletaire@gmail.com>

--- src/components/Account/Menu.tsx
+++ src/components/Account/Menu.tsx
@@ -29,7 +29,7 @@ const AccountMenu: React.FC<BoxProps> = (props) => {
   if (isLoading) {
     return (
       <Box {...props}>
-        <Spinner />
+        <Spinner minH='35px' minW='35px' />
       </Box>
     )
   }
