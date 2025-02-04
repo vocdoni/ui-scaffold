d478a46d98777d8bc18509f46cfeaead67e6dc14
Òscar Casajuana <elboletaire@gmail.com>
2025-02-04 16:21:36 +0100

    refs #893 There's no need for a wrapping box...

--- src/components/Layout/AlertIcon.tsx
+++ src/components/Layout/AlertIcon.tsx
@@ -1,4 +1,4 @@
-import { AlertIconProps, Box, chakra, useAlertContext, useAlertStyles } from '@chakra-ui/react'
+import { AlertIconProps, chakra, useAlertContext, useAlertStyles } from '@chakra-ui/react'
 import { cx } from '@chakra-ui/utils'
 import { cloneElement } from 'react'
 import { FaRegCheckCircle } from 'react-icons/fa'
@@ -19,19 +19,18 @@ export const AlertIcon = (props: AlertIconProps) => {
   const icon = icons[status] || <RiErrorWarningLine />
 
   return (
-    <Box gridRowStart={1} gridRowEnd={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-      <chakra.span
-        display='inherit'
-        data-status={status}
-        {...props}
-        className={cx('chakra-alert__icon', props.className)}
-        __css={css}
-        {...props}
-      >
-        {cloneElement(icon, {
-          color: css.color,
-        })}
-      </chakra.span>
-    </Box>
+    <chakra.span
+      display='inherit'
+      data-status={status}
+      alignSelf='center'
+      {...props}
+      className={cx('chakra-alert__icon', props.className)}
+      __css={css}
+      {...props}
+    >
+      {cloneElement(icon, {
+        color: css.color,
+      })}
+    </chakra.span>
   )
 }
