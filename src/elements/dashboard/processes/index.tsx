562539c9071bac92dc3c7efeb697b5eeb96ca3fa
Gerard <g.roura.v@protonmail.com>
2025-02-26 11:47:15 +0100

    use organization.electionIndex to check if the organization has elections

--- src/elements/dashboard/processes/index.tsx
+++ src/elements/dashboard/processes/index.tsx
@@ -1,5 +1,6 @@
+import { useOrganization } from '@vocdoni/react-providers'
 import { ElectionListWithPagination } from '@vocdoni/sdk'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLoaderData, useOutletContext, useParams } from 'react-router-dom'
 import { DashboardContents } from '~components/Layout/Dashboard'
@@ -12,19 +13,17 @@ const OrganizationVotings = () => {
   const { setBack, setTitle } = useOutletContext<DashboardLayoutContext>()
   const data = useLoaderData() as ElectionListWithPagination
   const { status } = useParams<{ status?: string }>()
-  const { elections } = data
-  const [allElectionsLength, setAllElectionsLength] = useState<number>()
+  const { organization } = useOrganization()
 
   // Set page title
   useEffect(() => {
     setTitle(t('organization.votings_list', { defaultValue: 'Voting processes list' }))
     setBack(null)
-    setAllElectionsLength(elections.length)
   }, [setTitle, setBack])
 
   return (
     <DashboardContents display='flex' flexDirection='column'>
-      {!!allElectionsLength && <ProcessStatusFilter status={status} alignSelf='end' />}
+      {!!organization.electionIndex && <ProcessStatusFilter status={status} alignSelf='end' />}
       <Votings data={data as ElectionListWithPagination} status={status} />
     </DashboardContents>
   )
