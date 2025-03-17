ede52202ac7c3d6881047beecfb0b9aac368c4a8
Òscar Casajuana <elboletaire@gmail.com>
2025-03-17 17:04:51 +0100

    style(coib): recover alert icon color

--- src/theme/components/alert.ts
+++ src/theme/components/alert.ts
@@ -16,32 +16,6 @@ const baseStyle = (props: any) => {
   const color = statusColorMap[status] || statusColorMap['info']
 
   return {
-    container: {
-      display: 'grid',
-      gridTemplateColumns: { base: '1fr', lg: 'fit-content(50px) 1fr' },
-      border: '1px solid',
-      borderColor: 'alert.border',
-      borderRadius: 'lg',
-      width: 'fit-content',
-      bgColor: 'alert.bg.light',
-      _dark: {
-        bgColor: 'alert.bg.dark',
-      },
-    },
-    title: {
-      gridColumn: { lg: 2 },
-      color: 'alert.color.light',
-      _dark: {
-        color: 'alert.color.dark',
-      },
-    },
-    description: {
-      gridColumn: { lg: 2 },
-      color: 'alert.color.light',
-      _dark: {
-        color: 'alert.color.dark',
-      },
-    },
     icon: {
       color,
       w: 6,
