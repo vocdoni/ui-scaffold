8508b57daaa1e82407153598bbb99297aa31338b
Òscar Casajuana <elboletaire@gmail.com>
2025-04-10 13:41:15 +0200

    chore(auth): remove unnecessary (and nonworking) code

--- src/components/Account/LogoutBtn.tsx
+++ src/components/Account/LogoutBtn.tsx
@@ -1,7 +1,6 @@
 import { Button, ButtonProps } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
-import { useAccount } from 'wagmi'
 import { useAuth } from '~components/Auth/useAuth'
 import { Routes } from '~src/router/routes'
 
@@ -9,14 +8,10 @@ const LogoutBtn = (props?: ButtonProps) => {
   const { t } = useTranslation()
   const { logout: authLogout } = useAuth()
   const navigate = useNavigate()
-  const { connector } = useAccount()
 
   const logout = () => {
     setTimeout(async () => {
       authLogout()
-      //TODO the next line does not work.
-      // The objective is to do the WAGMI
-      await connector.disconnect()
     }, 100)
     navigate(Routes.root)
   }
