f81709b6b5203176eaa7d0b43e16892124f0497e
Alexander Arce <alexander@vocdoni.org>
2025-05-20 15:45:37 +0200

    refactor(subscription): 💳 replace Subscription component with SubscriptionPlans

--- src/components/Organization/Dashboard/Subscription.tsx
+++ src/components/Organization/Dashboard/Subscription.tsx
@@ -1,10 +1,7 @@
-import { useTranslation } from 'react-i18next'
-import { Subscription } from '~components/Organization/Subscription'
+import { SubscriptionPlans } from '~components/Pricing/Plans'
 
 const SubscriptionPage = () => {
-  const { t } = useTranslation()
-
-  return <Subscription />
+  return <SubscriptionPlans />
 }
 
 export default SubscriptionPage
