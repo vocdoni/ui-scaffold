1069526a0d7502f24f7c35b04b2028c0e2b84a0d
Òscar Casajuana <elboletaire@gmail.com>
2025-05-27 17:09:00 +0200

    style(theme): dark theme improvements
    refs #1061

--- src/theme/components/menu.ts
+++ src/theme/components/menu.ts
@@ -3,26 +3,28 @@ import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
 
 const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(menuAnatomy.keys)
 
-const baseStyle = definePartsStyle({
+const baseStyle = definePartsStyle((props) => ({
   list: {
     p: 1,
     borderRadius: 'sm',
     boxShadow: 'md',
+    bg: props.colorMode === 'dark' ? 'black.800' : 'white',
   },
   item: {
+    bg: props.colorMode === 'dark' ? 'black.800' : 'white',
     _selected: {
-      bg: 'gray.200',
-      color: 'black',
+      bg: props.colorMode === 'dark' ? 'black.650' : 'gray.100',
+      color: props.colorMode === 'dark' ? 'white' : 'black',
     },
     _focus: {
-      bg: 'gray.100',
-      color: 'black',
+      bg: props.colorMode === 'dark' ? 'black.700' : 'gray.100',
+      color: props.colorMode === 'dark' ? 'white' : 'black',
     },
   },
 
   divider: {
     m: 0,
   },
-})
+}))
 
 export const Menu = defineMultiStyleConfig({ baseStyle })
