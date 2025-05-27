0a0aee2b47e0f77607376f7e830106a7dec4882e
Òscar Casajuana <elboletaire@gmail.com>
2025-05-27 17:09:07 +0200

    refactor(navbar): languages list cleanup
    refs #1061

--- src/theme/selectStyles.ts
+++ src/theme/selectStyles.ts
@@ -1,45 +1,21 @@
 import { ChakraStylesConfig } from 'chakra-react-select'
 
 export const selectStyles: ChakraStylesConfig = {
-  control: (base) => ({
+  container: (base) => ({
     ...base,
-    padding: 0,
-    fontSize: 'sm',
-    borderRadius: 'sm',
-    border: '1px solid rgb(228, 228, 231) !important',
-    paddingRight: 2,
-    paddingLeft: 2,
-    _focusVisible: {
-      border: 'none',
-    },
+    border: 'none',
   }),
   dropdownIndicator: (base) => ({
     ...base,
-    padding: '0px',
-    width: '2px',
-    color: 'rgb(113, 113, 122)',
+    m: 0,
   }),
   valueContainer: (base) => ({
     ...base,
     p: 0,
-    mr: 2,
-  }),
-  option: (base) => ({
-    ...base,
-    padding: '8px',
-    fontSize: 'sm',
-    fontWeight: 'normal',
-    bgColor: 'white',
-    p: 1,
-    borderRadius: 'sm',
-
-    _hover: {
-      bgColor: 'hsl(240 4.8% 95.9%)',
-    },
+    mr: 0,
   }),
   menu: (base) => ({
     ...base,
-    zIndex: 10,
     minW: 'fit-content',
   }),
   menuList: (base) => ({
