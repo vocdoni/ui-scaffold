5d0cb0367ae4e216a187b0fbd92be4afb78f4fda
Òscar Casajuana <elboletaire@gmail.com>
2025-05-30 11:11:38 +0200

    refactor(theme): extract semantic color tokens and update pricing card layout
    refs #1061

---
+++ src/theme/semanticColors.ts
@@ -0,0 +1,116 @@
+/**
+ * Semantic colors allow us to define colors based on their usage, specially the theme
+ * https://v2.chakra-ui.com/docs/styled-system/semantic-tokens
+ */
+
+const chakra = {
+  body: {
+    bg: {
+      _dark: 'black.650',
+      _light: 'white',
+    },
+  },
+}
+
+const texts = {
+  primary: {
+    _light: 'black',
+    _dark: 'white',
+  },
+  subtle: {
+    _light: 'gray.500',
+    _dark: 'gray.400',
+  },
+}
+
+const semanticColors = {
+  chakra,
+  auth: {
+    bg: {
+      _dark: 'black.550',
+      _light: 'gray.50',
+    },
+    card: {
+      bg: {
+        _dark: 'black',
+        _light: 'white',
+      },
+      border: 'gray.200',
+    },
+  },
+  card: {
+    pricing: {
+      bg: {
+        default: chakra.body.bg._light,
+        _dark: chakra.body.bg._dark,
+      },
+      border: {
+        default: 'gray.200',
+        _dark: 'black.700',
+      },
+      current: {
+        bg: {
+          default: 'gray.100',
+          _dark: 'black.600',
+        },
+        color: {
+          default: 'black',
+          _dark: 'white',
+        },
+      },
+      featured: {
+        badge: {
+          bg: {
+            default: 'black',
+            _dark: 'white',
+          },
+          color: {
+            default: 'white',
+            _dark: 'black',
+          },
+        },
+        border: {
+          default: 'black',
+          _dark: 'white',
+        },
+      },
+    },
+  },
+  input: {
+    placeholder: {
+      _light: 'gray.500',
+      _dark: 'gray.400',
+    },
+  },
+  table: {
+    border: {
+      _light: 'gray.200',
+      _dark: 'black.700',
+    },
+  },
+  tabs: {
+    tab: {
+      color: {
+        _light: 'gray.500',
+        _dark: 'gray.400',
+      },
+      active: {
+        color: {
+          _light: 'black',
+          _dark: 'white',
+        },
+        bg: {
+          _light: 'white',
+          _dark: 'black',
+        },
+      },
+    },
+    bg: {
+      _light: 'gray.100',
+      _dark: 'black.700',
+    },
+  },
+  texts,
+}
+
+export default semanticColors
