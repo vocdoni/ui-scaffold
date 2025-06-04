f65a9cd8e63ab244a8090056e95feeb8b8009807
Alexander Arce <ander.arce161@gmail.com>
2025-06-04 15:31:00 +0200

    feat: wire up Members page with table provider

--- src/elements/dashboard/members.tsx
+++ src/elements/dashboard/members.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useOutletContext } from 'react-router-dom'
-import { DashboardContents } from '~components/Layout/Dashboard'
+import { useLoaderData, useOutletContext } from 'react-router-dom'
 import { Memberbase } from '~components/Members'
+import { MembersTableProvider } from '~components/Members/MembersTableProvider'
+import { DashboardContents } from '~components/shared/Dashboard/Contents'
 import { DashboardLayoutContext } from '~elements/LayoutDashboard'
 import { Routes } from '~routes'
 
 const Members = () => {
   const { t } = useTranslation()
   const { setBreadcrumb } = useOutletContext<DashboardLayoutContext>()
+  const data = useLoaderData()
 
   // Set layout variables
   useEffect(() => {
@@ -16,9 +18,11 @@ const Members = () => {
   }, [setBreadcrumb])
 
   return (
-    <DashboardContents>
-      <Memberbase />
-    </DashboardContents>
+    <MembersTableProvider>
+      <DashboardContents>
+        <Memberbase />
+      </DashboardContents>
+    </MembersTableProvider>
   )
 }
 
