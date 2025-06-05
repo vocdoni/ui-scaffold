6398124927c284ac50c6af4998b791c08f7af2cc
Alexander Arce <ander.arce161@gmail.com>
2025-06-05 16:26:08 +0200

    feat(members): enhance member deletion with modal and toast
    - Added DeleteMemberModal component for bulk or individual deletion confirmation.
    - Display toast on successful deletion or failure.
    - Replaced inline deletes with modal confirmation.

--- src/components/Memberbase/Members/index.tsx
+++ src/components/Memberbase/Members/index.tsx
@@ -20,6 +20,11 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Progress,
   Table,
   TableContainer,
@@ -30,11 +35,13 @@ import {
   Thead,
   Tr,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 import { enforceHexPrefix, useOrganization } from '@vocdoni/react-providers'
 import { Trans, useTranslation } from 'react-i18next'
 import { LuEllipsis, LuEye, LuEyeOff, LuPlus, LuSearch, LuSettings, LuTrash2, LuUsers } from 'react-icons/lu'
+import { useRevalidator } from 'react-router-dom'
 import { ApiEndpoints } from '~components/Auth/api'
 import { useAuth } from '~components/Auth/useAuth'
 import { QueryKeys } from '~src/queries/keys'
@@ -43,20 +50,13 @@ import { ExportMembers } from './Export'
 import { ImportMembers } from './Import'
 import { MemberManager } from './Manager'
 
-export type Participant = {
-  [key: string]: string
-}
-
-type OrganizationParticipantsResponse = {
-  participants?: Participant[]
-}
-
 const useDeleteParticipants = () => {
   const { bearedFetch } = useAuth()
   const { organization } = useOrganization()
-  const client = useQueryClient()
+  const { revalidate } = useRevalidator()
 
   return useMutation<void, Error, string[]>({
+    mutationKey: [QueryKeys.organization.participants(organization?.address)],
     mutationFn: async (participantIDs: string[]) =>
       await bearedFetch<void>(
         ApiEndpoints.OrganizationParticipants.replace('{address}', enforceHexPrefix(organization.address)),
@@ -68,20 +68,13 @@ const useDeleteParticipants = () => {
         }
       ),
     onSuccess: () => {
-      client.invalidateQueries({
-        queryKey: QueryKeys.organization.participants(organization?.address),
-      })
+      revalidate()
     },
   })
 }
 
-const MemberActions = ({ participant }) => {
+const MemberActions = ({ participant, onDelete }) => {
   const { t } = useTranslation()
-  const deleteMutation = useDeleteParticipants()
-
-  const handleDelete = () => {
-    deleteMutation.mutate([participant.id])
-  }
 
   return (
     <Menu placement='bottom-end'>
@@ -92,7 +85,7 @@ const MemberActions = ({ participant }) => {
           control={<MenuItem>{t('members_table.edit', { defaultValue: 'Edit' })}</MenuItem>}
         />
         <MenuDivider />
-        <MenuItem color='red.400' onClick={handleDelete}>
+        <MenuItem color='red.400' onClick={onDelete}>
           {t('members_table.delete', { defaultValue: 'Delete' })}
         </MenuItem>
       </MenuList>
@@ -172,16 +165,11 @@ const MemberFilters = ({ globalFilter, setGlobalFilter }) => {
   )
 }
 
-const MemberBulkActions = ({ selectedParticipants }) => {
+const MemberBulkActions = ({ selectedParticipants, onDelete }) => {
   const { t } = useTranslation()
-  const deleteMutation = useDeleteParticipants()
 
   const createGroup = () => {
     console.log('Creating group with IDs:', selectedParticipants)
-  }
-
-  const handleBulkDelete = () => {
-    deleteMutation.mutate(selectedParticipants)
   }
 
   return (
@@ -201,13 +189,7 @@ const MemberBulkActions = ({ selectedParticipants }) => {
               defaultValue: 'Create a group',
             })}
           </Button>
-          <Button
-            leftIcon={<Icon as={LuTrash2} />}
-            size='sm'
-            colorScheme='red'
-            variant='outline'
-            onClick={handleBulkDelete}
-          >
+          <Button leftIcon={<Icon as={LuTrash2} />} size='sm' colorScheme='red' variant='outline' onClick={onDelete}>
             {t('members_table.bulk_delete', { defaultValue: 'Delete' })}
           </Button>
         </>
@@ -220,93 +202,187 @@ const MemberBulkActions = ({ selectedParticipants }) => {
   )
 }
 
+const DeleteMemberModal = ({ isOpen, onClose, ...props }) => {
+  const { t } = useTranslation()
+  const toast = useToast()
+  const deleteMutation = useDeleteParticipants()
+  const { selectedRows, setSelectedRows } = useTable()
+
+  const handleDelete = async () => {
+    try {
+      await deleteMutation.mutateAsync(selectedRows)
+      setSelectedRows([])
+      toast({
+        title: t('memberbase.delete_member.success', {
+          defaultValue: 'Member deleted successfully',
+          defaultValue_other: 'Members deleted successfully',
+          count: selectedRows.length,
+        }),
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+      onClose()
+    } catch (error) {
+      toast({
+        title: t('memberbase.delete_member.error', {
+          defaultValue: 'Error deleting member',
+          defaultValue_other: 'Error deleting members',
+          count: selectedRows.length,
+        }),
+        description: error.message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size='lg' {...props}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Flex flexDirection='column' gap={2}>
+            <Heading fontSize='lg'>{t('memberbase.delete_member.title', { defaultValue: 'Delete Members' })}</Heading>
+            <Text fontSize='sm' size='sm' color='texts.subtle'>
+              {t('memberbase.delate_member.subtitle', {
+                defaultValue: 'Are you sure you want to delete {{count}} member? This action cannot be undone.',
+                defaultValue_other: 'Are you sure you want to delete {{count}} members? This action cannot be undone.',
+                count: selectedRows.length,
+              })}
+            </Text>
+          </Flex>
+        </ModalHeader>
+        <ModalBody>
+          <Flex justifyContent='flex-end' mt={4} gap={2}>
+            <Button variant='outline' onClick={onClose}>
+              {t('memberbase.delete_member.cancel', { defaultValue: 'Cancel' })}
+            </Button>
+            <Button isLoading={deleteMutation.isPending} colorScheme='red' onClick={handleDelete}>
+              {t('memberbase.delete_member.delete', { defaultValue: 'Delete' })}
+            </Button>
+          </Flex>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
+
 const MembersTable = () => {
   const { t } = useTranslation()
   const {
     filteredData,
     isLoading,
-    globalFilter,
-    setGlobalFilter,
+    search,
+    setSearch,
     selectedRows,
+    setSelectedRows,
+    isSelected,
     allVisibleSelected,
     someSelected,
     toggleAll,
     toggleOne,
     columns,
   } = useTable()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const openDeleteModal = (participant?) => {
+    if (participant) {
+      setSelectedRows((prev) => [...prev, participant])
+    }
+    onOpen()
+  }
 
   if (isLoading) return <Progress isIndeterminate />
 
   return (
-    <TableContainer border='1px' borderRadius='sm' borderColor='table.border'>
-      <Flex px={4} pt={4}>
-        <Flex direction='column' flex={1} gap={2}>
-          <MemberFilters globalFilter={globalFilter} setGlobalFilter={setGlobalFilter} />
-          <MemberBulkActions selectedParticipants={selectedRows} />
+    <>
+      <TableContainer border='1px' borderRadius='sm' borderColor='table.border'>
+        <Flex px={4} pt={4}>
+          <Flex direction='column' flex={1} gap={2}>
+            <MemberFilters globalFilter={search} setGlobalFilter={setSearch} />
+            <MemberBulkActions selectedParticipants={selectedRows} onDelete={() => openDeleteModal()} />
+          </Flex>
+          <Flex gap={2}>
+            <ImportMembers />
+            <ExportMembers />
+            <MemberManager
+              control={
+                <Button colorScheme='black' leftIcon={<Icon as={LuPlus} />}>
+                  {t('memberbase.add_member.button', { defaultValue: 'Add Member' })}
+                </Button>
+              }
+            />
+          </Flex>
         </Flex>
-        <Flex gap={2}>
-          <ImportMembers />
-          <ExportMembers />
-          <MemberManager
-            control={
-              <Button leftIcon={<Icon as={LuPlus} />}>
-                {t('memberbase.add_member.button', { defaultValue: 'Add Member' })}
-              </Button>
-            }
-          />
-        </Flex>
-      </Flex>
 
-      <Table>
-        <Thead>
-          <Tr>
-            <Th width='50px'>
-              <Checkbox
-                isChecked={allVisibleSelected}
-                isIndeterminate={someSelected && !allVisibleSelected}
-                onChange={(e) => toggleAll(e.target.checked)}
-              />
-            </Th>
-            {columns
-              .filter((column) => column.visible)
-              .map((column) => (
-                <Th key={column.id}>{column.label}</Th>
-              ))}
-            <Th width='50px'>
-              <ColumnManager />
-            </Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {filteredData.map((participant) => (
-            <Tr key={participant.id}>
-              <Td>
-                <Checkbox
-                  isChecked={selectedRows.some((selected) => selected.id === participant.id)}
-                  onChange={(e) => toggleOne(participant, e.target.checked)}
-                />
-              </Td>
-              {columns
-                .filter((column) => column.visible)
-                .map((column) => (
-                  <Td key={column.id}>{participant[column.id]}</Td>
+        {filteredData.length === 0 ? (
+          <Flex justify='center' align='center' height='200px'>
+            <Text color='texts.subtle' fontSize='sm'>
+              {search
+                ? t('members_table.no_filter_results', {
+                    defaultValue: 'No members matching these attributes',
+                  })
+                : t('members_table.no_results', { defaultValue: 'No members found' })}
+            </Text>
+          </Flex>
+        ) : (
+          <>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th width='50px'>
+                    <Checkbox
+                      isChecked={allVisibleSelected}
+                      isIndeterminate={someSelected && !allVisibleSelected}
+                      onChange={(e) => toggleAll(e.target.checked)}
+                    />
+                  </Th>
+                  {columns
+                    .filter((column) => column.visible)
+                    .map((column) => (
+                      <Th key={column.id}>{column.label}</Th>
+                    ))}
+                  <Th width='50px'>
+                    <ColumnManager />
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {filteredData.map((participant) => (
+                  <Tr key={participant.id}>
+                    <Td>
+                      <Checkbox
+                        isChecked={isSelected(participant.participantNo)}
+                        onChange={(e) => toggleOne(participant.participantNo, e.target.checked)}
+                      />
+                    </Td>
+                    {columns
+                      .filter((column) => column.visible)
+                      .map((column) => (
+                        <Td key={column.id}>{participant[column.id]}</Td>
+                      ))}
+                    <Td>
+                      <MemberActions participant={participant} onDelete={() => openDeleteModal(participant)} />
+                    </Td>
+                  </Tr>
                 ))}
-              <Td>
-                <MemberActions participant={participant} />
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+              </Tbody>
+            </Table>
 
-      <Flex justify='space-between' p={4}>
-        <Text>Page 1</Text>
-        <HStack>
-          <IconButton aria-label='Previous' icon={<ChevronLeftIcon />} isDisabled />
-          <IconButton aria-label='Next' icon={<ChevronRightIcon />} isDisabled />
-        </HStack>
-      </Flex>
-    </TableContainer>
+            <Flex justify='space-between' p={4}>
+              <Text>Page 1</Text>
+              <HStack>
+                <IconButton aria-label='Previous' icon={<ChevronLeftIcon />} isDisabled />
+                <IconButton aria-label='Next' icon={<ChevronRightIcon />} isDisabled />
+              </HStack>
+            </Flex>
+          </>
+        )}
+      </TableContainer>
+      <DeleteMemberModal isOpen={isOpen} onClose={onClose} />
+    </>
   )
 }
 
