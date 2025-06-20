ea1ad7c9efde5e70a56ff620b1e4e51c0f3a147d
Alexander Arce <ander.arce161@gmail.com>
2025-06-20 11:25:22 +0200

    feat(process-create): enable configuration of multiple choice questions with min/max selection

--- src/components/Process/Create/MainContent/Questions/QuestionForm.tsx
+++ src/components/Process/Create/MainContent/Questions/QuestionForm.tsx
@@ -1,21 +1,24 @@
 import {
   Box,
   Button,
+  Checkbox,
   FormControl,
   FormErrorMessage,
   HStack,
   Icon,
   IconButton,
   Input,
+  Radio,
   Text,
   Textarea,
   VStack,
 } from '@chakra-ui/react'
 import { Select } from 'chakra-react-select'
 import { useEffect } from 'react'
-import { Controller, useFieldArray, useFormContext, useWatch } from 'react-hook-form'
+import { Controller, useFieldArray, useFormContext } from 'react-hook-form'
 import { Trans, useTranslation } from 'react-i18next'
 import { LuPlus, LuX } from 'react-icons/lu'
+import { useProcessTemplates } from '~components/Process/TemplateProvider'
 import { DashboardBox, DashboardSection } from '~components/shared/Dashboard/Contents'
 import { QuestionTypes } from '../..'
 
@@ -25,12 +28,12 @@ interface QuestionFormProps {
 }
 
 const SelectionLimits = ({ index }) => {
-  const { control, setValue } = useFormContext()
+  const { control, setValue, watch } = useFormContext()
   const { t } = useTranslation()
-  const questionType = useWatch({ name: 'questionType', control })
-  const fields = useWatch({ name: `questions.${index}.options`, control })
-  const min = useWatch({ name: `questions.${index}.minSelections`, control })
-  const max = useWatch({ name: `questions.${index}.maxSelections`, control })
+  const questionType = watch('questionType')
+  const fields = watch(`questions.${index}.options`)
+  const min = watch(`questions.${index}.minSelections`)
+  const max = watch(`questions.${index}.maxSelections`)
 
   // Ensure max is not less than min
   useEffect(() => {
@@ -117,19 +120,21 @@ const SelectionLimits = ({ index }) => {
 }
 
 export const QuestionForm = ({ index, onRemove }: QuestionFormProps) => {
-  const { control } = useFormContext()
   const { t } = useTranslation()
+  const { activeTemplate, placeholders } = useProcessTemplates()
   const {
     register,
     formState: { errors },
     setValue,
+    watch,
   } = useFormContext()
   const { fields, append, remove } = useFieldArray({
     name: `questions.${index}.options`,
   })
-  const min = useWatch({ name: `questions.${index}.minSelections`, control })
-  const max = useWatch({ name: `questions.${index}.maxSelections`, control })
-  const questionType = useWatch({ name: 'questionType', control })
+  const min = watch(`questions.${index}.minSelections`)
+  const max = watch(`questions.${index}.maxSelections`)
+  const questionType = watch('questionType')
+  const questions = watch('questions')
 
   useEffect(() => {
     if (min && max && max < min) {
@@ -142,12 +147,22 @@ export const QuestionForm = ({ index, onRemove }: QuestionFormProps) => {
       <Box>
         {/* Question title and description */}
         <HStack align='stretch' spacing={4}>
-          <VStack flex='1' align='stretch' spacing={4} mb={6}>
+          <VStack flex='1' align='stretch' spacing={2} mb={6}>
+            <Text fontSize='sm' color='texts.subtle'>
+              {t('process.create.question.question_number', {
+                defaultValue: 'Question {{index}} of {{total}}',
+                index: index + 1,
+                total: questions.length,
+              })}
+            </Text>
             <FormControl isInvalid={!!errors.questions?.[index]?.title}>
               <Input
                 px={0}
                 variant='unstyled'
-                placeholder={t('process_create.question.title.placeholder', 'Add a title to the question')}
+                placeholder={
+                  placeholders[activeTemplate]?.questions?.[index]?.title ??
+                  t('process_create.question.title.placeholder', 'Add a title to the question')
+                }
                 fontSize='lg'
                 fontWeight='bold'
                 {...register(`questions.${index}.title`, {
@@ -158,16 +173,19 @@ export const QuestionForm = ({ index, onRemove }: QuestionFormProps) => {
             </FormControl>
             <Textarea
               variant='unstyled'
-              placeholder={t(
-                'process.create.question.description.placeholder',
-                'Add the description of the question here (optional)...'
-              )}
+              placeholder={
+                placeholders[activeTemplate]?.questions?.[index]?.description ??
+                t(
+                  'process_create.question.description.placeholder',
+                  'Add the description of the question here (optional)...'
+                )
+              }
               resize='none'
               minH='60px'
               {...register(`questions.${index}.description`)}
             />
           </VStack>
-          {index > 0 && (
+          {questions.length > 1 && (
             <IconButton
               icon={<Icon as={LuX} />}
               aria-label={t('process_create.question.remove', 'Remove question')}
@@ -184,12 +202,20 @@ export const QuestionForm = ({ index, onRemove }: QuestionFormProps) => {
         {/* Options */}
         <VStack align='stretch' spacing={2}>
           {fields.map((field, optionIndex) => (
-            <HStack key={field.id}>
-              <FormControl isInvalid={!!errors.questions?.[index]?.options?.[optionIndex]?.text}>
+            <HStack key={field.id} align='start'>
+              {questionType === QuestionTypes.Single ? (
+                <Radio isChecked={false} isReadOnly mt={2} />
+              ) : (
+                <Checkbox isChecked={false} isReadOnly mt={2} />
+              )}
+              <FormControl isInvalid={!!errors.questions?.[index]?.options?.[optionIndex]?.text} flex='1'>
                 <Input
-                  placeholder={t('process_create.option.placeholder', 'Option {{number}}', {
-                    number: optionIndex + 1,
-                  })}
+                  placeholder={
+                    placeholders[activeTemplate]?.questions?.[index].options?.[optionIndex] ??
+                    t('process_create.option.placeholder', 'Option {{number}}', {
+                      number: optionIndex + 1,
+                    })
+                  }
                   {...register(`questions.${index}.options.${optionIndex}.text`, {
                     required: t('form.error.required', 'This field is required'),
                   })}
@@ -198,13 +224,16 @@ export const QuestionForm = ({ index, onRemove }: QuestionFormProps) => {
                   {errors.questions?.[index]?.options?.[optionIndex]?.text?.message?.toString()}
                 </FormErrorMessage>
               </FormControl>
-              <IconButton
-                icon={<Icon as={LuX} />}
-                aria-label={t('process_create.option.remove', 'Remove option')}
-                onClick={() => remove(optionIndex)}
-                size='sm'
-                variant='ghost'
-              />
+              {fields.length > 2 && (
+                <IconButton
+                  icon={<Icon as={LuX} />}
+                  aria-label={t('process_create.option.remove', 'Remove option')}
+                  onClick={() => remove(optionIndex)}
+                  size='sm'
+                  variant='ghost'
+                  mt={1}
+                />
+              )}
             </HStack>
           ))}
           <Button
