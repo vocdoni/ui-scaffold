27720ebda51d6abc22613cc10b42fce67353e19f
Alexander Arce <ander.arce161@gmail.com>
2025-06-20 11:27:21 +0200

    feat(process-create): wrap page in TemplateProvider and handle dirty form leave confirmation

--- src/elements/dashboard/processes/create.tsx
+++ src/elements/dashboard/processes/create.tsx
@@ -1,10 +1,15 @@
 import { PricingModalProvider } from '~components/Pricing/PricingModalProvider'
 import ProcessCreate from '~components/Process/Create'
+import { TemplateProvider } from '~components/Process/TemplateProvider'
 
-const ProcessCreatePage = () => (
-  <PricingModalProvider>
-    <ProcessCreate />
-  </PricingModalProvider>
-)
+const ProcessCreatePage = () => {
+  return (
+    <PricingModalProvider>
+      <TemplateProvider>
+        <ProcessCreate />
+      </TemplateProvider>
+    </PricingModalProvider>
+  )
+}
 
 export default ProcessCreatePage
