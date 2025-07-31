4d060df71f5b0bb651d6e0a05f5ce15d3ffac15a
Alexander Arce <ander.arce161@gmail.com>
2025-07-31 15:29:03 +0200

    style(QuestionForm): Fixed styling issue in light theme (#1116)

--- src/components/shared/Form/Select.tsx
+++ src/components/shared/Form/Select.tsx
@@ -1,6 +1,7 @@
 import { Select as ChakraSelect } from 'chakra-react-select'
+import { forwardRef } from 'react'
 import { selectStyles } from '~theme/selectStyles'
 
-export const Select = (props) => {
-  return <ChakraSelect chakraStyles={selectStyles} {...props} />
-}
+export const Select = forwardRef<any, any>(function Select(props, ref) {
+  return <ChakraSelect ref={ref} chakraStyles={selectStyles} {...props} />
+})
