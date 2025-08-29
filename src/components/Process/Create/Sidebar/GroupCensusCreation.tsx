818b2f384aef53e058cf251d58ced2787a1eea9e
Òscar Casajuana <elboletaire@gmail.com>
2025-08-29 11:14:31 +0200

    fix(census): cannot create processes with non CSP censuses

--- src/components/Process/Create/Sidebar/GroupCensusCreation.tsx
+++ src/components/Process/Create/Sidebar/GroupCensusCreation.tsx
@@ -2,14 +2,17 @@ import { Box, FormControl, FormErrorMessage, Input } from '@chakra-ui/react'
 import { useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { VoterAuthentication } from '../VoterAuthentication'
-import { GroupSelect } from './CensusCreation'
+import { CensusTypes, GroupSelect } from './CensusCreation'
 
 export const GroupCensusCreation = () => {
   const { t } = useTranslation()
   const {
     register,
+    watch,
     formState: { errors },
   } = useFormContext()
+
+  const censusType = watch('censusType')
 
   return (
     <Box display='flex' flexDirection='column' gap={4}>
@@ -20,7 +23,10 @@ export const GroupCensusCreation = () => {
         <Input
           type='hidden'
           {...register('censusId', {
-            required: t('form.error.census_config_required', 'Please configure the census authentication settings.'),
+            required: {
+              value: censusType === CensusTypes.CSP,
+              message: t('form.error.census_config_required', 'Please configure the census authentication settings.'),
+            },
           })}
         />
         <FormErrorMessage>{errors.censusId?.message?.toString()}</FormErrorMessage>
