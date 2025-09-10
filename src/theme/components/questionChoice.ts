603586ca33899bd249a37c0ee6499a2c2b223773
Alexander Arce <ander.arce161@gmail.com>
2025-09-10 09:24:39 +0200

    style: extended info public vote display (#1189)

---
+++ src/theme/components/questionChoice.ts
@@ -0,0 +1,38 @@
+import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
+import { questionChoiceAnatomy } from '@vocdoni/chakra-components'
+
+const { defineMultiStyleConfig, definePartsStyle } = createMultiStyleConfigHelpers(questionChoiceAnatomy)
+
+const baseStyle = definePartsStyle({
+  skeleton: {
+    w: '100%',
+    h: '150px',
+  },
+
+  image: {
+    w: '100%',
+    h: '100%',
+    borderTopRadius: 'lg',
+  },
+
+  label: {
+    '.chakra-skeleton + &': {
+      wordBreak: 'break-word',
+      fontWeight: 'semibold',
+      mt: 4,
+      px: 4,
+      mb: 4,
+    },
+  },
+
+  description: {
+    '.chakra-skeleton ~ &': {
+      px: 4,
+      mb: 4,
+    },
+  },
+})
+
+export const QuestionChoice = defineMultiStyleConfig({
+  baseStyle,
+})
