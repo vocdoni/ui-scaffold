6527f1137f404ff407e67ee4b1660479d72291c1
Alexander Arce <ander.arce161@gmail.com>
2025-09-16 15:31:04 +0200

    feat(CensusCreation): Disable group selector and auth configuration button if no groups available, also updated no groups message

--- src/components/Process/Create/Sidebar/ExtraConfig.tsx
+++ src/components/Process/Create/Sidebar/ExtraConfig.tsx
@@ -1,88 +1,12 @@
-import { Box, FormControl, FormErrorMessage, FormLabel, Spinner, VStack } from '@chakra-ui/react'
-import { chakraComponents } from 'chakra-react-select'
-import { useEffect, useState } from 'react'
+import { Box, FormControl, FormErrorMessage, FormLabel, VStack } from '@chakra-ui/react'
 import { Controller, useFormContext } from 'react-hook-form'
 import { Trans, useTranslation } from 'react-i18next'
-import { CensusTypes } from '~components/Process/Census/CensusType'
 import { Select } from '~components/shared/Form/Select'
-import { useGroups } from '~src/queries/groups'
 import { useValidations } from '~utils/validation'
 
 type SelectOption<T = string> = {
   value: T
   label: string
-}
-
-export const GroupSelect = () => {
-  const { t } = useTranslation()
-  const {
-    watch,
-    control,
-    formState: { errors },
-  } = useFormContext()
-  const censusType = watch('censusType')
-  const [hasFetchedScroll, setHasFetchedScroll] = useState(false)
-  const { data, fetchNextPage, hasNextPage, isFetching } = useGroups(6)
-
-  const CustomMenuList = (props) => {
-    return (
-      <chakraComponents.MenuList {...props}>
-        {props.children}
-        {hasNextPage && (
-          <Box py={2} textAlign='center'>
-            {isFetching ? <Spinner size='sm' /> : t('process_create.groups.scroll_to_load', 'Scroll to load more...')}
-          </Box>
-        )}
-      </chakraComponents.MenuList>
-    )
-  }
-
-  // Reset hasFetchedScroll when fetching starts
-  useEffect(() => {
-    if (!isFetching) setHasFetchedScroll(false)
-  }, [isFetching])
-
-  return (
-    <FormControl isInvalid={!!errors.groupId}>
-      <FormLabel>
-        <Trans i18nKey='process_create.census.label'>Census creation</Trans>
-      </FormLabel>
-      <Controller
-        control={control}
-        name='groupId'
-        rules={{
-          required: {
-            value: censusType === CensusTypes.CSP,
-            message: t('form.error.required', 'This field is required'),
-          },
-        }}
-        render={({ field }) => {
-          const selected = data?.find((g) => g.id === field.value) ?? null
-          return (
-            <Select
-              options={data ?? []}
-              value={selected}
-              getOptionLabel={(option) => option.title}
-              getOptionValue={(option) => option.id}
-              placeholder={t('process_create.group.select', 'Select group')}
-              isLoading={isFetching}
-              onChange={(option) => field.onChange(option?.id ?? '')}
-              onMenuScrollToBottom={async () => {
-                if (hasNextPage && !hasFetchedScroll) {
-                  setHasFetchedScroll(true)
-                  await fetchNextPage()
-                }
-              }}
-              closeMenuOnSelect
-              maxMenuHeight={200}
-              components={{ MenuList: CustomMenuList }}
-            />
-          )
-        }}
-      />
-      <FormErrorMessage>{errors.groupId?.message?.toString()}</FormErrorMessage>
-    </FormControl>
-  )
 }
 
 export const ExtraConfig = () => {
