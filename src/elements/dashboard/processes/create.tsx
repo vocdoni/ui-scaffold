6426caee091da97b87dc3f839e42b993cf46775a
Alexander Arce <ander.arce161@gmail.com>
2025-11-12 14:55:15 +0100

    chore(ProcessCreate): added create process breadcrumb

--- src/elements/dashboard/processes/create.tsx
+++ src/elements/dashboard/processes/create.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useOutletContext } from 'react-router-dom'
 import { PricingModalProvider } from '~components/Pricing/PricingModalProvider'
 import ProcessCreate from '~components/Process/Create'
 import { TemplateProvider } from '~components/Process/Create/TemplateProvider'
+import { DashboardLayoutContext } from '~elements/LayoutDashboard'
 
 const ProcessCreatePage = () => {
+  const { t } = useTranslation()
+  const { setBreadcrumb } = useOutletContext<DashboardLayoutContext>()
+
+  useEffect(() => {
+    setBreadcrumb([
+      {
+        title: t('create_process', { defaultValue: 'Create process' }),
+      },
+    ])
+  }, [setBreadcrumb])
+
   return (
     <PricingModalProvider>
       <TemplateProvider>
